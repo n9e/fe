31b363884a698d14440b939d407cb5ba1325b569
jsers <xqing9024@gmail.com>
2023-06-06 10:48:15 +0800

    feat(bizGroup): business groups support rendering into a tree structure

--- src/pages/targets/BusinessGroup/index.tsx
+++ src/pages/targets/BusinessGroup/index.tsx
@@ -3,8 +3,8 @@ import { Resizable } from 're-resizable';
 import _ from 'lodash';
 import classNames from 'classnames';
 import { useHistory } from 'react-router-dom';
-import { Input } from 'antd';
-import { LeftOutlined, RightOutlined, SettingOutlined, SearchOutlined } from '@ant-design/icons';
+import { Input, Space } from 'antd';
+import { LeftOutlined, RightOutlined, SettingOutlined, SearchOutlined, DownOutlined } from '@ant-design/icons';
 import { useTranslation } from 'react-i18next';
 import { getBusiGroups } from '@/services/common';
 import { CommonStateContext } from '@/App';
@@ -17,6 +17,93 @@ interface IProps {
   renderHeadExtra?: () => React.ReactNode;
 }
 
+interface Node {
+  id: number;
+  title: string;
+  key: string | number;
+  children?: Node[];
+}
+
+export function listToTree(data: { id: number; name: string }[]) {
+  const treeData: Node[] = [];
+  _.forEach(data, (item) => {
+    const separatorIndex = item.name.indexOf('-');
+    if (separatorIndex > 0) {
+      const groupName = item.name.substring(0, separatorIndex);
+      const name = item.name.substring(separatorIndex + 1);
+      const group = _.find(treeData, { title: groupName });
+      if (group) {
+        if (group.children) {
+          group.children.push({
+            title: name,
+            key: item.id,
+            id: item.id,
+          });
+        } else {
+          group.children = [
+            {
+              title: name,
+              key: item.id,
+              id: item.id,
+            },
+          ];
+        }
+      } else {
+        const groupNodes = _.filter(data, (item) => {
+          return item.name.indexOf(`${groupName}-`) === 0;
+        });
+        if (groupNodes.length > 1) {
+          treeData.push({
+            title: groupName,
+            key: groupName,
+            id: item.id,
+            children: [
+              {
+                title: name,
+                key: item.id,
+                id: item.id,
+              },
+            ],
+          });
+        } else {
+          treeData.push({
+            title: item.name,
+            key: item.id,
+            id: item.id,
+          });
+        }
+      }
+    } else {
+      treeData.push({
+        title: item.name,
+        key: item.id,
+        id: item.id,
+      });
+    }
+  });
+  return treeData;
+}
+
+export function getLocaleCollapsedNodes() {
+  const val = localStorage.getItem('biz_group_collapsed');
+  try {
+    if (val) {
+      const parsed = JSON.parse(val);
+      if (_.isArray(parsed)) {
+        return parsed;
+      }
+      return [];
+    }
+    return [];
+  } catch (e) {
+    return [];
+  }
+}
+
+export function setLocaleCollapsedNodes(nodes: string[]) {
+  localStorage.setItem('biz_group_collapsed', JSON.stringify(nodes));
+}
+
 export default function index(props: IProps) {
   const { t } = useTranslation();
   const { title = t('common:business_group'), renderHeadExtra, curBusiId, setCurBusiId } = props;
@@ -25,6 +112,7 @@ export default function index(props: IProps) {
   const [width, setWidth] = useState(_.toNumber(localStorage.getItem('leftwidth') || 200));
   const { busiGroups } = useContext(CommonStateContext);
   const [businessGroupData, setBusinessGroupData] = useState<{ id: number; name: string }[]>([]);
+  const [collapsedNodes, setCollapsedNodes] = useState<string[]>(getLocaleCollapsedNodes());
 
   useEffect(() => {
     setBusinessGroupData(busiGroups);
@@ -76,24 +164,72 @@ export default function index(props: IProps) {
             }}
           />
           <div className='radio-list'>
-            {_.map(businessGroupData, (item) => {
-              return (
-                <div
-                  className={classNames({
-                    'n9e-metric-views-list-content-item': true,
-                    active: item.id == curBusiId,
-                  })}
-                  key={item.id}
-                  onClick={() => {
-                    if (item.id !== curBusiId) {
-                      localStorage.setItem('curBusiId', _.toString(item.id));
-                      setCurBusiId && setCurBusiId(item.id, item);
-                    }
-                  }}
-                >
-                  <span className='name'>{item.name}</span>
-                </div>
-              );
+            {_.map(listToTree(businessGroupData), (item) => {
+              if (item.children) {
+                return (
+                  <div className='n9e-biz-group-item n9e-biz-group-group' key={item.key}>
+                    <div
+                      className='name'
+                      onClick={() => {
+                        let newCollapsedNodes = _.cloneDeep(collapsedNodes);
+                        if (_.includes(newCollapsedNodes, item.key)) {
+                          newCollapsedNodes = _.without(newCollapsedNodes, item.key as string);
+                        } else {
+                          newCollapsedNodes.push(item.key as string);
+                        }
+                        setCollapsedNodes(newCollapsedNodes);
+                        setLocaleCollapsedNodes(newCollapsedNodes);
+                      }}
+                    >
+                      <Space>
+                        {item.title}
+                        {!_.includes(collapsedNodes, item.key) ? <DownOutlined /> : <RightOutlined />}
+                      </Space>
+                    </div>
+                    {!_.includes(collapsedNodes, item.key) && (
+                      <div className='children'>
+                        {_.map(item.children, (child) => {
+                          return (
+                            <div
+                              className={classNames({
+                                'n9e-biz-group-item': true,
+                                active: child.id == curBusiId,
+                              })}
+                              key={child.id}
+                              onClick={() => {
+                                if (child.id !== curBusiId) {
+                                  localStorage.setItem('curBusiId', _.toString(child.id));
+                                  setCurBusiId && setCurBusiId(child.id, child);
+                                }
+                              }}
+                            >
+                              <div className='name'>{child.title}</div>
+                            </div>
+                          );
+                        })}
+                      </div>
+                    )}
+                  </div>
+                );
+              } else {
+                return (
+                  <div
+                    className={classNames({
+                      'n9e-biz-group-item': true,
+                      active: item.id == curBusiId,
+                    })}
+                    key={item.key}
+                    onClick={() => {
+                      if (item.id !== curBusiId) {
+                        localStorage.setItem('curBusiId', _.toString(item.id));
+                        setCurBusiId && setCurBusiId(item.id, item);
+                      }
+                    }}
+                  >
+                    <div className='name'>{item.title}</div>
+                  </div>
+                );
+              }
             })}
           </div>
         </div>
