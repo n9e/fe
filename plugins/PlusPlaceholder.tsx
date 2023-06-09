81978458b45076f5f1f3d75128f2b372abd6436b
jsers <xqing9024@gmail.com>
2023-06-09 09:56:40 +0800

    refactor(login): update style

--- plugins/PlusPlaceholder.tsx
+++ plugins/PlusPlaceholder.tsx
@@ -37,5 +37,6 @@ function Jobs() {
 const advancedCates = [];
 const envCateMap = {};
 enum AdvancedDatasourceCateEnum {}
+const getLicense = async () => {};
 
-export { AlertRule, QueryBuilder, datasource, Event, EventLogs, EventPreview, Explorer, Jobs, advancedCates, envCateMap, AdvancedDatasourceCateEnum };
+export { AlertRule, QueryBuilder, datasource, Event, EventLogs, EventPreview, Explorer, Jobs, advancedCates, envCateMap, AdvancedDatasourceCateEnum, getLicense };
