0bcfafdb788a0b1400e90d515923c2e0457fa5c1
tanxiao <tanxiao@asiainfo.com>
2023-06-13 14:52:07 +0800

    feat: add verification code for login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -17,16 +17,18 @@
 import React, { useState, useEffect } from 'react';
 import { Form, Input, Button, message } from 'antd';
 import { useHistory, useLocation } from 'react-router-dom';
-import { UserOutlined, LockOutlined } from '@ant-design/icons';
-import { getSsoConfig, getRedirectURL, getRedirectURLCAS, getRedirectURLOAuth, authLogin } from '@/services/login';
+import { PictureOutlined, UserOutlined, LockOutlined } from '@ant-design/icons';
+import { getCaptcha, getSsoConfig, getRedirectURL, getRedirectURLCAS, getRedirectURLOAuth, authLogin } from '@/services/login';
 import './login.less';
 
 import { useTranslation } from 'react-i18next';
+import { verify } from 'crypto';
 export interface DisplayName {
   oidc: string;
   cas: string;
   oauth: string;
 }
+
 export default function Login() {
   const { t } = useTranslation();
   const [form] = Form.useForm();
@@ -39,6 +41,8 @@ export default function Login() {
     oauth: 'OAuth',
   });
 
+  const verifyimg = document.querySelector('#verify-img') as HTMLImageElement
+
   useEffect(() => {
     getSsoConfig().then((res) => {
       if (res.dat) {
@@ -49,20 +53,31 @@ export default function Login() {
         });
       }
     });
+    getCaptcha().then((res) => {
+        var img = document.querySelector('#verify-img') as HTMLImageElement
+        if (res.dat) {
+            img.src=res.dat.imgdata
+            localStorage.setItem('captchaid',res.dat.captchaid)
+        } else {
+          message.warning('获取验证码失败');
+        }
+      });
   }, []);
 
   const handleSubmit = async () => {
     try {
       await form.validateFields();
       login();
+      verifyimg.click()
     } catch {
       console.log(t('输入有误'));
     }
   };
 
   const login = async () => {
-    let { username, password } = form.getFieldsValue();
-    authLogin(username, password).then((res) => {
+    let { username, password, verifyvalue } = form.getFieldsValue();
+    let captchaid = localStorage.getItem('captchaid') || ""
+    authLogin(username, password, captchaid, verifyvalue).then((res) => {
       const { dat, err } = res;
       const { access_token, refresh_token } = dat;
       localStorage.setItem('access_token', access_token);
@@ -110,6 +125,36 @@ export default function Login() {
             >
               <Input type='password' placeholder={t('请输入密码')} onPressEnter={handleSubmit} prefix={<LockOutlined className='site-form-item-icon' />} />
             </Form.Item>
+
+            <Form.Item
+              label='验证码'
+              name='verifyvalue'
+              rules={[
+                {
+                  required: true,
+                  message: t('请输入验证码'),
+                },
+              ]}
+            >
+              <Input placeholder={t('请输入验证码')} onPressEnter={handleSubmit} prefix={<PictureOutlined className='site-form-item-icon' />} />
+            </Form.Item>
+
+
+            <div className='verifyimg-div'>
+              <img id='verify-img'
+                onClick={() => {
+                    getCaptcha().then((res) => {
+                    if (res.dat) {
+                        verifyimg.src=res.dat.imgdata
+                        localStorage.setItem('captchaid',res.dat.captchaid)
+                    } else {
+                      message.warning('获取验证码失败');
+                    }
+                  });
+                }}
+                alt='点击获取验证码'
+              />
+            </div>
 
             <Form.Item>
               <Button type='primary' onClick={handleSubmit}>
