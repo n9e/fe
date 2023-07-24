bf40c3f2b867a2bf67a3115cb780c7b75661259e
jsers <xqing9024@gmail.com>
2023-07-24 18:43:18 +0800

    refactor: update style

--- src/pages/explorer/components/FieldsSidebar/Field.tsx
+++ src/pages/explorer/components/FieldsSidebar/Field.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import _ from 'lodash';
-import { Popover, Progress } from 'antd';
+import { Popover, Progress, Spin } from 'antd';
 import { PlusCircleOutlined, CloseCircleOutlined } from '@ant-design/icons';
 import { getFieldLabel } from '../../Elasticsearch/utils';
 import { normalizeFieldValuesQueryRequestBody } from '../../Elasticsearch/utils';
@@ -25,6 +25,7 @@ export default function Field(props: Props) {
   const { form, timesRef, datasourceValue } = params;
   const [top5Visible, setTop5Visible] = useState<boolean>(false);
   const [top5Data, setTop5Data] = useState<any[]>([]);
+  const [top5Loading, setTop5Loading] = useState<boolean>(false);
   const fieldLabel = getFieldLabel(item, fieldConfig);
 
   return (
@@ -39,26 +40,29 @@ export default function Field(props: Props) {
       title={fieldLabel}
       content={
         <div className='es-discover-field-values-topn'>
-          <strong>前 5 个值</strong>
-          <div className='es-discover-field-values-topn-list'>
-            {_.map(top5Data, (item) => {
-              const percent = _.floor(item.value * 100, 2);
-              return (
-                <div key={item.label} className='es-discover-field-values-topn-item'>
-                  <div className='es-discover-field-values-topn-item-content'>
-                    <div className='es-discover-field-values-topn-item-label'>{item.label}</div>
-                    <div className='es-discover-field-values-topn-item-percent'>{percent}%</div>
+          <strong>Top 5 values</strong>
+          <Spin spinning={top5Loading}>
+            <div className='es-discover-field-values-topn-list'>
+              {_.map(top5Data, (item) => {
+                const percent = _.floor(item.value * 100, 2);
+                return (
+                  <div key={item.label} className='es-discover-field-values-topn-item'>
+                    <div className='es-discover-field-values-topn-item-content'>
+                      <div className='es-discover-field-values-topn-item-label'>{item.label || '(empty)'}</div>
+                      <div className='es-discover-field-values-topn-item-percent'>{percent}%</div>
+                    </div>
+                    <Progress percent={percent} size='small' showInfo={false} strokeColor='#6c53b1' />
                   </div>
-                  <Progress percent={percent} size='small' showInfo={false} strokeColor='#6c53b1' />
-                </div>
-              );
-            })}
-          </div>
+                );
+              })}
+            </div>
+          </Spin>
         </div>
       }
       onVisibleChange={(visible) => {
         setTop5Visible(visible);
         if (visible) {
+          setTop5Loading(true);
           const values = form.getFieldsValue();
           getFieldValues(
             datasourceValue,
@@ -74,9 +78,13 @@ export default function Field(props: Props) {
               item,
             ),
             item,
-          ).then((res) => {
-            setTop5Data(res);
-          });
+          )
+            .then((res) => {
+              setTop5Data(res);
+            })
+            .finally(() => {
+              setTop5Loading(false);
+            });
         } else {
           setTop5Data([]);
         }
