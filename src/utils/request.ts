918ce524930bf2e34eab1a2359f5e8116457bc3b
jsers <xqing9024@gmail.com>
2023-08-02 15:09:19 +0800

    code refactor

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -79,7 +79,7 @@ request.interceptors.response.use(
           // proxy/elasticsearch 返回的数据结构是 { ...data }
           // proxy/jeager 返回的数据结构是 { data: [], errors: [] }
           if (
-            _.some(['/api/n9e/proxy'], (item) => {
+            _.some(['/api/n9e/proxy', '/probe/v1'], (item) => {
               return url.includes(item);
             })
           ) {
