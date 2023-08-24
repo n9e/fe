80999b5646d28fafe9f6480a11806d210c320a5e
jsers <xqing9024@gmail.com>
2023-08-24 15:59:35 +0800

    refactor(explorer): timeseries explorer support multiple panels

--- src/pages/explorer/Metric.tsx
+++ src/pages/explorer/Metric.tsx
@@ -14,8 +14,9 @@
  * limitations under the License.
  *
  */
-import React from 'react';
-import { LineChartOutlined } from '@ant-design/icons';
+import React, { useState } from 'react';
+import { Button } from 'antd';
+import { LineChartOutlined, CloseCircleOutlined } from '@ant-design/icons';
 import _ from 'lodash';
 import { useTranslation } from 'react-i18next';
 import PageLayout from '@/components/pageLayout';
@@ -24,10 +25,43 @@ import './index.less';
 
 const MetricExplorerPage = () => {
   const { t } = useTranslation('explorer');
+  const [panels, setPanels] = useState([
+    {
+      uuid: _.uniqueId('panel_'),
+    },
+  ]);
+
   return (
     <PageLayout title={t('title')} icon={<LineChartOutlined />}>
       <div className='prometheus-page'>
-        <Explorer type='metric' defaultCate='prometheus' />
+        {_.map(panels, (panel, idx) => {
+          return (
+            <div key={panel.uuid} style={{ minHeight: 300, marginBottom: 16, position: 'relative' }}>
+              <Explorer type='metric' defaultCate='prometheus' panelIdx={idx} />
+              {panels.length > 1 && (
+                <CloseCircleOutlined
+                  style={{
+                    position: 'absolute',
+                    top: 8,
+                    right: 8,
+                    fontSize: 14,
+                  }}
+                  onClick={() => {
+                    setPanels(_.filter(panels, (item) => item.uuid !== panel.uuid));
+                  }}
+                />
+              )}
+            </div>
+          );
+        })}
+        <Button
+          style={{ width: '100%' }}
+          onClick={() => {
+            setPanels([...panels, { uuid: _.uniqueId('panel_') }]);
+          }}
+        >
+          Add Panel
+        </Button>
       </div>
     </PageLayout>
   );
