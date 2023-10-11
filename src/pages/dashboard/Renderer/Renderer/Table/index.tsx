dc30a1a0739ec536ead19fcd1b237054be1a8b30
jsers <xqing9024@gmail.com>
2023-10-11 09:42:32 +0800

    refactor(dashboard-table): change local cache key

--- src/pages/dashboard/Renderer/Renderer/Table/index.tsx
+++ src/pages/dashboard/Renderer/Renderer/Table/index.tsx
@@ -392,7 +392,7 @@ function TableCpt(props: IProps, ref: any) {
     }, [JSON.stringify(columns), displayMode, JSON.stringify(calculatedValues), sortObj, themeMode, aggrDimension, overrides, size]),
     columnsState: {
       persistenceType: 'localStorage',
-      persistenceKey: `dashboard-table-resizable-${values.id}`,
+      persistenceKey: `dashboard-table2-resizable-${values.id}`,
     },
   });
 
