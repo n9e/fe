4f967ef2e6c5a913492c5a266695211fbce08045
jsers <xqing9024@gmail.com>
2023-10-13 09:56:52 +0800

    refactor: business group selector show search #88

--- src/pages/alertRulesBuiltin/Import.tsx
+++ src/pages/alertRulesBuiltin/Import.tsx
@@ -108,7 +108,7 @@ function Import(props: IProps & ModalWrapProps) {
             },
           ]}
         >
-          <Select>
+          <Select showSearch optionFilterProp='children'>
             {_.map(busiGroups, (item) => {
               return (
                 <Select.Option key={item.id} value={item.id}>
