8f6e16b7906904a634c1adc0d08ef4503f386f0f
jsers <xqing9024@gmail.com>
2023-12-13 15:34:27 +0800

    refactor: 快捷查询固定 X轴时间范围

--- src/pages/monitor/object/metricViews/Graph.tsx
+++ src/pages/monitor/object/metricViews/Graph.tsx
@@ -381,7 +381,7 @@ export default function Graph(props: IProps) {
         </Space>
       </div>
       <div>
-        {chartType === 'line' && <Timeseries inDashboard={false} values={lineGraphProps as any} series={series} />}
+        {chartType === 'line' && <Timeseries inDashboard={false} values={lineGraphProps as any} series={series} time={range} />}
         {chartType === 'hexbin' && (
           <div style={{ padding: '20px 0 0 0', height: highLevelConfig.chartheight }}>
             <Hexbin values={hexbinGraphProps as any} series={series} time={range} />
