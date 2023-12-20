1b08f580807fe630d78bb9ce67b67902b6ab4851
jsers <xqing9024@gmail.com>
2023-12-20 15:47:18 +0800

    refactor: update site settings

--- src/pages/siteSettings/locale/zh_CN.ts
+++ src/pages/siteSettings/locale/zh_CN.ts
@@ -1,11 +1,14 @@
 const zh_CN = {
   title: '站点设置',
+  link_icon_url: '链接图标',
+  favicon_url: '网站图标 ( Favicon )',
+  favicon_url_tip: '输入图片资源地址，建议尺寸 32x32',
   login_page_logo_url: '登录页 Logo',
   login_page_logo_url_tip: '输入图片资源地址，建议尺寸比例 120x38',
   menu_big_logo_url: '菜单页大 Logo',
-  menu_big_logo_url_tip: '输入图片资源地址，建议尺寸比例 36x38',
-  menu_small_logo_url: '菜单页小 Logo & 站点图标',
-  menu_small_logo_url_tip: '输入图片资源地址，建议尺寸 100x100',
+  menu_big_logo_url_tip: '输入图片资源地址，建议尺寸比例 120x38',
+  menu_small_logo_url: '菜单页小 Logo',
+  menu_small_logo_url_tip: '输入图片资源地址，建议尺寸 38x38',
   document_url: '文档地址',
   document_url_tip: '覆盖默认的夜莺文档链接地址',
   page_title: '站点标题',
@@ -20,5 +23,7 @@ const zh_CN = {
   businessGroupSeparator: '业务组分隔符',
   teamDisplayMode: '团队展示模式',
   teamSeparator: '团队分隔符',
+  darkTheme: '深色主题',
+  lightTheme: '浅色主题',
 };
 export default zh_CN;
