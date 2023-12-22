72ebfe4d6f28da4373e1d2fa3bd9cdce0065b7e3
jsers <xqing9024@gmail.com>
2023-12-22 18:15:39 +0800

    code refactor

--- src/components/menu/SideMenu/index.tsx
+++ src/components/menu/SideMenu/index.tsx
@@ -311,7 +311,7 @@ const SideMenu = () => {
       >
         <div
           className={cn(
-            'z-20 flex h-full select-none flex-col justify-between border-0 transition-width',
+            'z-20 flex h-full select-none flex-col justify-between border-0 border-r border-solid border-fc-300 transition-width',
             collapsed ? 'w-[64px]' : uncollapsedWidth,
             collapsedHover ? `absolute ${uncollapsedWidth} shadow-mf` : '',
           )}
