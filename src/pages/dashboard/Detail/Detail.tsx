e90dbb2480bb093266744b86038e27af6e9830fa
jsers <xqing9024@gmail.com>
2024-01-30 11:18:25 +0800

    refactor: optimize dashboard layout style

--- src/pages/dashboard/Detail/Detail.tsx
+++ src/pages/dashboard/Detail/Detail.tsx
@@ -220,56 +220,7 @@ export default function DetailV2(props: IProps) {
   }, 2000);
 
   return (
-    <PageLayout
-      customArea={
-        <Title
-          isPreview={isPreview}
-          isBuiltin={isBuiltin}
-          isAuthorized={isAuthorized}
-          gobackPath={gobackPath}
-          dashboard={dashboard}
-          range={range}
-          setRange={(v) => {
-            setRange(v);
-          }}
-          onAddPanel={(type) => {
-            if (type === 'row') {
-              const newPanels = updatePanelsInsertNewPanelToGlobal(
-                panels,
-                {
-                  type: 'row',
-                  id: uuidv4(),
-                  name: i18n.language === 'en_US' ? 'Row' : '分组',
-                  collapsed: true,
-                },
-                'row',
-              );
-              setPanels(newPanels);
-              handleUpdateDashboardConfigs(dashboard.id, {
-                configs: panelsMergeToConfigs(dashboard.configs, newPanels),
-              });
-            } else {
-              setEditorData({
-                visible: true,
-                id: uuidv4(),
-                initialValues: {
-                  name: 'Panel Title',
-                  type,
-                  targets: [
-                    {
-                      refId: 'A',
-                      expr: '',
-                    },
-                  ],
-                  custom: defaultCustomValuesMap[type],
-                  options: defaultOptionsValuesMap[type],
-                },
-              });
-            }
-          }}
-        />
-      }
-    >
+    <PageLayout customArea={<div />}>
       <div className='dashboard-detail-container'>
         <div className='dashboard-detail-content scroll-container' ref={containerRef}>
           <Affix
@@ -283,6 +234,52 @@ export default function DetailV2(props: IProps) {
                 display: query.viewMode !== 'fullscreen' ? 'block' : 'none',
               }}
             >
+              <Title
+                isPreview={isPreview}
+                isBuiltin={isBuiltin}
+                isAuthorized={isAuthorized}
+                gobackPath={gobackPath}
+                dashboard={dashboard}
+                range={range}
+                setRange={(v) => {
+                  setRange(v);
+                }}
+                onAddPanel={(type) => {
+                  if (type === 'row') {
+                    const newPanels = updatePanelsInsertNewPanelToGlobal(
+                      panels,
+                      {
+                        type: 'row',
+                        id: uuidv4(),
+                        name: i18n.language === 'en_US' ? 'Row' : '分组',
+                        collapsed: true,
+                      },
+                      'row',
+                    );
+                    setPanels(newPanels);
+                    handleUpdateDashboardConfigs(dashboard.id, {
+                      configs: panelsMergeToConfigs(dashboard.configs, newPanels),
+                    });
+                  } else {
+                    setEditorData({
+                      visible: true,
+                      id: uuidv4(),
+                      initialValues: {
+                        name: 'Panel Title',
+                        type,
+                        targets: [
+                          {
+                            refId: 'A',
+                            expr: '',
+                          },
+                        ],
+                        custom: defaultCustomValuesMap[type],
+                        options: defaultOptionsValuesMap[type],
+                      },
+                    });
+                  }
+                }}
+              />
               {!editable && (
                 <div style={{ padding: '0px 10px', marginBottom: 8 }}>
                   <Alert type='warning' message='仪表盘已经被别人修改，为避免相互覆盖，请刷新仪表盘查看最新配置和数据' />
