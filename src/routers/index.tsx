30e206cecb2d14ea92d04ef3c293f878ba8bc8fb
guguji5 <604821528@qq.com>
2024-02-27 11:36:19 +0800

    feat: add out-of-service page when crash

--- src/routers/index.tsx
+++ src/routers/index.tsx
@@ -21,6 +21,7 @@ import _ from 'lodash';
 import { getMenuPerm } from '@/services/common';
 import { CommonStateContext } from '@/App';
 import Page403 from '@/pages/notFound/Page403';
+import OutOfService from '@/pages/notFound/OutOfService';
 import NotFound from '@/pages/notFound';
 import Login from '@/pages/login';
 import Overview from '@/pages/login/overview';
@@ -220,6 +221,7 @@ export default function Content() {
         </Route>
         <Route path='/403' component={Page403} />
         <Route path='/404' component={NotFound} />
+        <Route path='/out-of-service' component={OutOfService} />
         <Route path='*' component={NotFound} />
       </Switch>
     </div>
