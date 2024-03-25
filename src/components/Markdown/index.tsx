fe97c007de1d8af64fc98f1c83ba697b6c3bfd08
jsers <xqing9024@gmail.com>
2024-03-25 15:57:53 +0800

    refactor: update Markdown component

--- src/components/Markdown/index.tsx
+++ src/components/Markdown/index.tsx
@@ -14,43 +14,59 @@
  * limitations under the License.
  *
  */
-import React from 'react';
+import React, { useContext } from 'react';
 import ReactMarkdown from 'react-markdown';
 import gfm from 'remark-gfm';
 import rehypeRaw from 'rehype-raw';
 import classNames from 'classnames';
-
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { CommonStateContext } from '@/App';
 import './index.less';
+
 interface IMarkDownPros {
   content: string;
   style?: any;
 }
 
+dark['pre[class*="language-"]'] = {
+  ...dark['pre[class*="language-"]'],
+  background: '#161b22',
+  border: '0 none',
+  'box-shadow': 'none',
+};
+
 // https://github.com/vitejs/vite/issues/3592 bug solve 记录
-const Markdown: React.FC<IMarkDownPros> = ({ content, style = {} }) => (
-  <div className='markdown-wrapper' style={style}>
-    <ReactMarkdown
-      remarkPlugins={[gfm]}
-      children={content}
-      rehypePlugins={[rehypeRaw]}
-      components={{
-        code({ node, inline, className, children, ...props }) {
-          return (
-            <code
-              {...props}
-              className={classNames({
-                [className || '']: !!className,
-                'base-code': true,
-                'base-code-inline': inline,
-              })}
-            >
-              {children}
-            </code>
-          );
-        },
-      }}
-    />
-  </div>
-);
+const Markdown: React.FC<IMarkDownPros> = ({ content, style = {} }) => {
+  const { darkMode } = useContext(CommonStateContext);
+
+  return (
+    <div className='markdown-wrapper' style={style}>
+      <ReactMarkdown
+        remarkPlugins={[gfm]}
+        children={content}
+        rehypePlugins={[rehypeRaw]}
+        components={{
+          code({ node, inline, className, children, ...props }) {
+            const match = /language-(\w+)/.exec(className || '');
+            return !inline && match ? (
+              <SyntaxHighlighter {...props} children={String(children).replace(/\n$/, '')} language={match[1]} PreTag='div' style={darkMode ? dark : undefined} />
+            ) : (
+              <div
+                className={classNames({
+                  [className || '']: !!className,
+                  'base-code': true,
+                  'base-code-inline': inline,
+                })}
+              >
+                <code {...props}>{children}</code>
+              </div>
+            );
+          },
+        }}
+      />
+    </div>
+  );
+};
 
 export default Markdown;
--- src/pages/dashboardBuiltin/Instructions/index.tsx
+++ src/pages/dashboardBuiltin/Instructions/index.tsx
@@ -1,27 +1,12 @@
-import React, { useState, useEffect, useContext } from 'react';
-import ReactMarkdown from 'react-markdown';
-import rehypeRaw from 'rehype-raw';
-import gfm from 'remark-gfm';
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
-import classNames from 'classnames';
-import { CommonStateContext } from '@/App';
+import React, { useState, useEffect } from 'react';
+import Markdown from '@/components/Markdown';
 import { getInstructionsByName } from '../services';
-import './style.less';
-
-dark['pre[class*="language-"]'] = {
-  ...dark['pre[class*="language-"]'],
-  background: '#161b22',
-  border: '0 none',
-  'box-shadow': 'none',
-};
 
 interface Props {
   name: string;
 }
 
 export default function Instructions(props: Props) {
-  const { darkMode } = useContext(CommonStateContext);
   const { name } = props;
   const [data, setData] = useState<string>('');
 
@@ -35,32 +20,7 @@ export default function Instructions(props: Props) {
 
   return (
     <div>
-      <ReactMarkdown
-        className='instructions-markdown-body'
-        rehypePlugins={[rehypeRaw]}
-        remarkPlugins={[gfm]}
-        components={{
-          code({ node, inline, className, children, ...props }) {
-            const match = /language-(\w+)/.exec(className || '');
-            return !inline && match ? (
-              <SyntaxHighlighter {...props} children={String(children).replace(/\n$/, '')} language={match[1]} PreTag='div' style={darkMode ? dark : undefined} />
-            ) : (
-              <code
-                {...props}
-                className={classNames({
-                  [className || '']: !!className,
-                  'base-code': true,
-                  'base-code-inline': inline,
-                })}
-              >
-                {children}
-              </code>
-            );
-          },
-        }}
-      >
-        {data || 'No Data'}
-      </ReactMarkdown>
+      <Markdown content={data || 'No Data'}></Markdown>
     </div>
   );
 }
