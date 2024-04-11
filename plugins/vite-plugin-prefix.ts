668e9b5e0e536e601e0bcc793dd06a3e2bc052cd
star-6 <290043518@qq.com>
2024-04-11 11:35:45 +0800

    update:plugin-prefix

--- plugins/vite-plugin-prefix.ts
+++ plugins/vite-plugin-prefix.ts
@@ -8,7 +8,6 @@ export default function prefixPlugin(prefix: string) {
     transform(code, id) {
       if (id.endsWith('tsx')) {
         const ast = jscodeshift(code);
-        const prefixValue = jscodeshift.literal(prefix);
         ast.find(jscodeshift.CallExpression).forEach((path) => {
           const callee = path.node.callee;
           if (
@@ -23,20 +22,12 @@ export default function prefixPlugin(prefix: string) {
             props.forEach((prop) => {
               if (prop.key.name === 'src') {
                 if (prop.value.type === 'Identifier' || prop.value.type === 'MemberExpression') {
-                  // 如果 src 是变量或表达式
-                  // const prefixValue = 'MemberExpression';
                   prop.value = jscodeshift.template.expression`${jscodeshift.literal(prefix)} + ${prop.value}`;
                 } else if (prop.value.type === 'TemplateLiteral') {
-                  // 如果 src 是模板字符串
-                  // const prefixValue = 'TemplateLiteral';
                   prop.value = jscodeshift.template.expression`\`\${${jscodeshift.literal(prefix)}}\${${prop.value}}\``;
                 } else if (prop.value.type === 'LogicalExpression' || prop.value.type === 'ConditionalExpression') {
-                  // 如果 src 是逻辑表达式或条件表达式
-                  // const prefixValue = 'LogicalExpression';
                   prop.value = jscodeshift.template.expression`${jscodeshift.literal(prefix)} + ${prop.value}`;
                 } else if (prop.value.type === 'Literal') {
-                  // 如果 src 是字符串字面量
-                  // const prefixValue = 'Literal';
                   prop.value.value = `${prefix}${prop.value.value}`;
                 }
               }
