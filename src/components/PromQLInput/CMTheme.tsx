40e112519b35fbb7202691ebdca4bca9e9c54c19
jsers <xqing9024@gmail.com>
2024-04-24 15:24:54 +0800

    refactor: update Built-in Metrics

--- src/components/PromQLInput/CMTheme.tsx
+++ src/components/PromQLInput/CMTheme.tsx
@@ -26,7 +26,7 @@ export const baseTheme = EditorView.theme({
   },
   '.cm-scroller': {
     overflow: 'hidden',
-    fontFamily: 'Consolas,Helvetica Neue,sans-serif,PingFangSC-Regular,microsoft yahei ui,microsoft yahei,simsun,"sans-serif"',
+    fontFamily: 'Consolas,Monaco,sans-serif,PingFangSC-Regular,microsoft yahei ui,microsoft yahei,simsun,"sans-serif"',
   },
   '.cm-matchingBracket': {
     fontWeight: 'bold',
