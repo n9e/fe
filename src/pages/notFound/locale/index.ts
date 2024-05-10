bd26deb8cef72bd4999ce553df9a565d4139b770
guguji5 <604821528@qq.com>
2024-05-10 15:22:37 +0800

    refactor: translate out of service to i18n

---
+++ src/pages/notFound/locale/index.ts
@@ -0,0 +1,8 @@
+import i18next from 'i18next';
+import en_US from './en_US';
+import zh_CN from './zh_CN';
+import zh_HK from './zh_HK';
+
+i18next.addResourceBundle('en_US', 'notFound', en_US);
+i18next.addResourceBundle('zh_CN', 'notFound', zh_CN);
+i18next.addResourceBundle('zh_HK', 'notFound', zh_HK);
---
+++ src/pages/notFound/locale/zh_CN.ts
@@ -0,0 +1,6 @@
+const zh_CN = {
+    'The Flashcat webpage is': "Flashcat网页端",
+    'The Nightingale webpage is': "夜莺监控网页端",
+    'out of Service': "服务不可用"
+};
+export default zh_CN;
