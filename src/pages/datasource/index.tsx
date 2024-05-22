50b998d81faeb6498ed61dab7216ade2385cd001
jsers <xqing9024@gmail.com>
2024-05-22 10:22:04 +0800

    update datasource page

--- src/pages/datasource/index.tsx
+++ src/pages/datasource/index.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import _ from 'lodash';
+import { Input, Button, Modal } from 'antd';
+import { useDebounce } from 'ahooks';
 import { useTranslation } from 'react-i18next';
 import PageLayout from '@/components/pageLayout';
 import { allCates } from '@/components/AdvancedWrap/utils';
@@ -9,6 +11,7 @@ import TableSource from './components/TableSource';
 import Detail from './Detail';
 import Form from './Form';
 import './locale';
+import { SearchOutlined } from '@ant-design/icons';
 
 export { Form };
 
@@ -17,6 +20,9 @@ export default function index() {
   const [pluginList, setPluginList] = useState<any[]>();
   const [detailVisible, setDetailVisible] = useState(false);
   const [detailData, setDetailData] = useState();
+  const [searchVal, setSearchVal] = useState<string>('');
+  const debouncedSearchValue = useDebounce(searchVal, { wait: 500 });
+  const [chooseDataSourceTypeModalVisible, setChooseDataSourceTypeModalVisible] = useState(false);
 
   useEffect(() => {
     getDataSourcePluginList().then((res) => {
@@ -36,28 +42,72 @@ export default function index() {
 
   return (
     <PageLayout title={t('title')}>
-      <div className='srm'>
-        <SourceCards sourceMap={pluginList} urlPrefix='help/source' />
-        <div className='page-title'>{t('list_title')}</div>
-        {pluginList && (
-          <TableSource
-            pluginList={pluginList}
-            nameClick={(record) => {
-              setDetailVisible(true);
-              setDetailData(record);
+      <div>
+        <div
+          className='n9e-border-base'
+          style={{
+            padding: 16,
+          }}
+        >
+          <div
+            className='mb2'
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
             }}
-          />
-        )}
-        {detailVisible && (
-          <Detail
-            visible={detailVisible}
-            data={detailData}
-            onClose={() => {
-              setDetailVisible(false);
-            }}
-          />
-        )}
+          >
+            <Input
+              prefix={<SearchOutlined />}
+              style={{ width: 300 }}
+              placeholder={t('search_placeholder')}
+              value={searchVal}
+              onChange={(e) => {
+                setSearchVal(e.target.value);
+              }}
+            />
+            <Button
+              type='primary'
+              onClick={() => {
+                setChooseDataSourceTypeModalVisible(true);
+              }}
+            >
+              {t('common:btn.add')}
+            </Button>
+          </div>
+          {pluginList && (
+            <TableSource
+              debouncedSearchValue={debouncedSearchValue}
+              pluginList={pluginList}
+              nameClick={(record) => {
+                setDetailVisible(true);
+                setDetailData(record);
+              }}
+            />
+          )}
+          {detailVisible && (
+            <Detail
+              visible={detailVisible}
+              data={detailData}
+              onClose={() => {
+                setDetailVisible(false);
+              }}
+            />
+          )}
+        </div>
       </div>
+      <Modal
+        title={t('chooseDataSourceType')}
+        visible={chooseDataSourceTypeModalVisible}
+        destroyOnClose
+        width={960}
+        footer={null}
+        onCancel={() => {
+          setChooseDataSourceTypeModalVisible(false);
+        }}
+      >
+        <SourceCards sourceMap={pluginList} urlPrefix='help/source' />
+      </Modal>
     </PageLayout>
   );
 }
