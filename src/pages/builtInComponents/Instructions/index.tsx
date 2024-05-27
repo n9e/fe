d37d80f62a5ce040330deb303d5cd1da17d4cd0a
jsers <xqing9024@gmail.com>
2024-05-27 11:44:51 +0800

    refactor: remove anchor links

--- src/pages/builtInComponents/Instructions/index.tsx
+++ src/pages/builtInComponents/Instructions/index.tsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import _ from 'lodash';
 import MDEditor from '@uiw/react-md-editor';
 import { CommonStateContext } from '@/App';
 
@@ -6,15 +7,20 @@ interface Props {
   readme?: string;
 }
 
-const Markdown = MDEditor.Markdown as any;
-
 export default function Instructions(props: Props) {
   const { darkMode } = useContext(CommonStateContext);
   const { readme } = props;
 
   return (
     <div className='builtin-w-md-editor' data-color-mode={darkMode ? 'dark' : 'light'}>
-      <Markdown source={readme} />
+      <MDEditor.Markdown
+        source={readme}
+        rehypeRewrite={(node: any) => {
+          if (_.includes(['h1', 'h2', 'h3', 'h4', 'h5', 'h6'], node.tagName)) {
+            node.children = node.children.filter((item) => item.tagName != 'a');
+          }
+        }}
+      />
     </div>
   );
 }
