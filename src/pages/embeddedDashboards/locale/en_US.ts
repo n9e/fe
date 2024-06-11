f4797a511f4477214dea1246d6e41af247e23462
jsers <xqing9024@gmail.com>
2024-06-11 19:05:39 +0800

    refactor: update embedded-dashboards

--- src/pages/embeddedDashboards/locale/en_US.ts
+++ src/pages/embeddedDashboards/locale/en_US.ts
@@ -1,7 +1,10 @@
 const en_US = {
   title: 'Embedded Dashboards',
   edit_title: 'Edit Embedded Dashboards',
+  edit_btn: 'Configure Now',
   name: 'Name',
+  name_msg: 'Please enter name',
   url: 'URL',
+  url_msg: 'Please enter URL',
 };
 export default en_US;
--- src/pages/embeddedDashboards/locale/zh_CN.ts
+++ src/pages/embeddedDashboards/locale/zh_CN.ts
@@ -1,6 +1,7 @@
 const zh_CN = {
   title: '集成仪表盘',
   edit_title: '编辑集成仪表盘',
+  edit_btn: '现在配置',
   name: '名称',
   name_msg: '请输入名称',
   url: '链接',
