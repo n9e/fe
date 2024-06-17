99ffaace52845d325315e06695f24d62aad80aa9
guguji5 <604821528@qq.com>
2024-06-17 15:51:26 +0800

    feat: out of service 优化为灰色，缓解焦虑

--- src/pages/notFound/locale/en_US.ts
+++ src/pages/notFound/locale/en_US.ts
@@ -1,5 +1,7 @@
 const en_US = {
-  
-}
+  网络开小差了: ' network has deviated',
+  可以刷新一下试试: 'You can try refreshing it',
+  刷新: 'Refresh',
+};
 
 export default en_US;
--- src/pages/notFound/locale/zh_HK.ts
+++ src/pages/notFound/locale/zh_HK.ts
@@ -1,7 +1,7 @@
 const zh_HK = {
-  'The Flashcat webpage is': "Flashcat網頁端",
-  'The Nightingale webpage is': "夜鶯監控網頁端",
-  'out of Service': "服務不可用"
+  "网络开小差了": "網絡開小差了",
+  "可以刷新一下试试": "可以刷新一下試試",
+  "刷新": "刷新",
 }
 
 export default zh_HK;
