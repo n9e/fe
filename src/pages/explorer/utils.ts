89e91eb603ed4583b052a1d78ad1acd9c1fae47b
guguji5 <604821528@qq.com>
2024-06-18 14:55:59 +0800

    refactor: es 日志查询缓存和url中参数对齐后再判断是否命中缓存

--- src/pages/explorer/utils.ts
+++ src/pages/explorer/utils.ts
@@ -150,7 +150,14 @@ export const formValuesIsInItems = (
       if (formValues.query.query !== undefined) {
         return _.isEqual(_.omit(itemFormValues?.query, ['range']), _.omit(formValues.query, ['range']));
       }
-      return _.isEqual(_.omit(itemFormValues?.query, ['query', 'range']), _.omit(formValues.query, ['query', 'range']));
+      if (formValues.datasourceCate === 'es') {
+        // es数据源区分index和indexPattern，无法严格equal，所以以缓存中的formValues.query中的keys为标准，逐个对比是否相等
+        const omitedFormValuesQuery = _.omit(formValues.query, ['query', 'range'])
+        const keys = _.keys(omitedFormValuesQuery);
+        const pickedKeysItemFormValues = _.pick(itemFormValues?.query, keys);
+        return _.isEqual(pickedKeysItemFormValues, omitedFormValuesQuery);
+      }
+      return _.isEqual(_.omit(itemFormValues?.query, ['query', 'range', 'index', 'date_field']), _.omit(formValues.query, ['query', 'range', 'index', 'date_field']));
     }
   });
 };
