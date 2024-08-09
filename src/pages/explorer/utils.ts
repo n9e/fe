8600a71a9f4fc3ace1a9d858f1541c5b27324a9b
jsers <xqing9024@gmail.com>
2024-08-09 18:00:23 +0800

    code refactor

--- src/pages/explorer/utils.ts
+++ src/pages/explorer/utils.ts
@@ -150,6 +150,12 @@ export const getFormValuesBySearch = (params: { [index: string]: string | null }
     if (data_source_name === 'volc-tls') {
       const project_id = _.get(params, 'project_id');
       const topic_id = _.get(params, 'topic_id');
+      const range_start = _.get(params, 'start');
+      const range_end = _.get(params, 'end');
+      const defaultRange =
+        range_start && range_end
+          ? { start: !isMathString(range_start) ? moment(Number(range_start)) : range_start, end: !isMathString(range_end) ? moment(Number(range_end)) : range_end }
+          : undefined;
       if (project_id && topic_id) {
         return {
           ...formValues,
@@ -157,6 +163,7 @@ export const getFormValuesBySearch = (params: { [index: string]: string | null }
             project_id,
             topic_id,
             query: queryString,
+            range: defaultRange,
           },
         };
       }
