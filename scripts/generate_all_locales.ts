20b43cb44e07cf1c84899d55a28d7a2142a97d10
jsers <xqing9024@gmail.com>
2024-09-18 17:34:02 +0800

    chore: update i18n scripts

--- scripts/generate_all_locales.ts
+++ scripts/generate_all_locales.ts
@@ -1,4 +1,8 @@
 /**
+ * 脚本执行注意事项：
+ * 1. package.json 添加 "type": "module"
+ * 2. npx ts-node generate_all_locales.ts
+ *
  * 在当前目录生成一个完整的语言文件 n9e_locale.json, 用于翻译其他语言
  * 语言包来源于
  * 1. src/locales/*\/zh_CN.ts
@@ -6,55 +10,92 @@
  * 3. src/pages/*\/locale/zh_CN.ts
  * 4. src/pages/help/*\/locale/zh_CN.ts
  * 5. src/pages/warning/*\/locale/zh_CN.ts
+ * 6. src/plus/components/*\/locale/zh_CN.ts
+ * 7. src/plus/datasource/*\/locale/zh_CN.ts
+ * 8. src/plus/pages/*\/locale/zh_CN.ts
+ * 9. src/plus/parcels/*\/locale/zh_CN.ts
  *
- * package.json 添加 "type": "module"
- * npx ts-node generate_all_locales.ts
  */
 
 import fs from 'fs';
 import path from 'path';
-import glob from 'glob';
+import * as glob from 'glob';
 import { fileURLToPath } from 'url';
+import lodash from 'lodash';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const targets = [
-  path.resolve(__dirname, '../src/locales/*/zh_CN.ts'),
-  path.resolve(__dirname, '../src/components/*/locale/zh_CN.ts'),
-  path.resolve(__dirname, '../src/pages/*/locale/zh_CN.ts'),
-  path.resolve(__dirname, '../src/pages/help/*/locale/zh_CN.ts'),
-  path.resolve(__dirname, '../src/pages/warning/*/locale/zh_CN.ts'),
-  path.resolve(__dirname, '../src/plus/datasource/*/locale/zh_CN.ts'),
+  {
+    path: path.resolve(__dirname, '../src/locales/*/zh_CN.ts'),
+    keys: ['locales'],
+  },
+  {
+    path: path.resolve(__dirname, '../src/components/*/locale/zh_CN.ts'),
+    keys: ['components'],
+  },
+  {
+    path: path.resolve(__dirname, '../src/pages/*/locale/zh_CN.ts'),
+    keys: ['pages'],
+  },
+  {
+    path: path.resolve(__dirname, '../src/pages/help/*/locale/zh_CN.ts'),
+    keys: ['pages', 'help'],
+  },
+  {
+    path: path.resolve(__dirname, '../src/pages/warning/*/locale/zh_CN.ts'),
+    keys: ['pages', 'warning'],
+  },
+  {
+    path: path.resolve(__dirname, '../src/plus/components/*/locale/zh_CN.ts'),
+    keys: ['plus', 'components'],
+  },
+  {
+    path: path.resolve(__dirname, '../src/plus/datasource/*/locale/zh_CN.ts'),
+    keys: ['plus', 'datasource'],
+  },
+  {
+    path: path.resolve(__dirname, '../src/plus/pages/*/locale/zh_CN.ts'),
+    keys: ['plus', 'pages'],
+  },
+  {
+    path: path.resolve(__dirname, '../src/plus/parcels/*/locale/zh_CN.ts'),
+    keys: ['plus', 'parcels'],
+  },
 ];
-const allfiles: string[] = [];
+
+const allfiles: {
+  files: string[];
+  keys: string[];
+}[] = [];
 
 targets.forEach((target) => {
-  const files = glob.sync(target);
-  allfiles.push(...files);
+  const files = glob.sync(target.path);
+  allfiles.push({
+    files,
+    keys: target.keys,
+  });
 });
 
 const allLocales: any = {};
 
-for await (const file of allfiles) {
-  const content = await import(file);
-  const arr = file.split('/');
-  let localeName = arr[arr.length - 2];
-  if (localeName === 'locale') {
-    localeName = arr[arr.length - 3];
-    let module = arr[arr.length - 4];
-    if (module === 'help' || module === 'warning') {
-      module = arr[arr.length - 5];
+for await (const item of allfiles) {
+  for await (const file of item.files) {
+    const content = await import(file);
+    const arr = file.split('/');
+    let localeName = arr[arr.length - 2];
+    if (localeName === 'locale') {
+      localeName = arr[arr.length - 3];
+      lodash.set(allLocales, [...item.keys, localeName], content.default);
+    } else {
+      lodash.set(allLocales, [...item.keys, localeName], content.default);
     }
-    allLocales[module] = allLocales[module] || {};
-    allLocales[module][localeName] = content.default;
-  } else {
-    allLocales[localeName] = content.default;
   }
 }
 
 const content = JSON.stringify(allLocales, null, 2);
 
-fs.writeFileSync(path.resolve(__dirname, 'n9e_locale.json'), content);
+fs.writeFileSync(path.resolve(__dirname, 'all_locales.json'), content);
 export {};
 
 // Path: scripts/generate_all_locales.js
---
+++ scripts/generate_each_locales.ts
@@ -0,0 +1,110 @@
+/**
+ * 脚本执行注意事项：
+ * 1. package.json 添加 "type": "module"
+ * 2. npx ts-node generate_each_locales.ts ${language}
+ * 比如 npx ts-node generate_each_locales.ts zh_CN
+ *
+ * 通过 all_locales.json 生成每个语言包
+ *
+ */
+
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import lodash from 'lodash';
+import { Project, SyntaxKind } from 'ts-morph';
+
+const lanugage = process.argv.slice(2)[0];
+if (lanugage) {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  const targets = [
+    {
+      path: path.resolve(__dirname, '../src/locales/*/zh_CN.ts'),
+      keys: ['locales'],
+    },
+    {
+      path: path.resolve(__dirname, '../src/components/*/locale/zh_CN.ts'),
+      keys: ['components'],
+    },
+    {
+      path: path.resolve(__dirname, '../src/pages/*/locale/zh_CN.ts'),
+      keys: ['pages'],
+    },
+    {
+      path: path.resolve(__dirname, '../src/pages/help/*/locale/zh_CN.ts'),
+      keys: ['pages', 'help'],
+    },
+    {
+      path: path.resolve(__dirname, '../src/pages/warning/*/locale/zh_CN.ts'),
+      keys: ['pages', 'warning'],
+    },
+    {
+      path: path.resolve(__dirname, '../src/plus/components/*/locale/zh_CN.ts'),
+      keys: ['plus', 'components'],
+    },
+    {
+      path: path.resolve(__dirname, '../src/plus/datasource/*/locale/zh_CN.ts'),
+      keys: ['plus', 'datasource'],
+    },
+    {
+      path: path.resolve(__dirname, '../src/plus/pages/*/locale/zh_CN.ts'),
+      keys: ['plus', 'pages'],
+    },
+    {
+      path: path.resolve(__dirname, '../src/plus/parcels/*/locale/zh_CN.ts'),
+      keys: ['plus', 'parcels'],
+    },
+  ];
+  function resolvePaths(paths: string[]): string {
+    return path.resolve(...paths);
+  }
+
+  /**
+   * 根据 all_locales.json 生成每个语言包
+   * 1. 读取 all_locales.json 内容
+   * 2. 遍历 targets 根据 keys 获取 all_locales.json 中对应的内容
+   * 3. 根据 keys 到对应的目录下生成 [lanugage].ts 文件
+   */
+  const localesJson = JSON.parse(fs.readFileSync(path.resolve(__dirname, 'all_locales.json'), 'utf-8'));
+  targets.forEach((target) => {
+    const key = target.keys.join('.');
+    const value = lodash.get(localesJson, key);
+    if (value) {
+      const valueKeys = lodash.keys(value);
+      valueKeys.forEach((valueKey) => {
+        const valueContent = value[valueKey];
+        const resolvedPath = resolvePaths([__dirname, '../src', ...target.keys, valueKey, ...(lodash.isEqual(target.keys, ['locales']) ? [] : ['locale']), `${lanugage}.json`]);
+        fs.writeFileSync(resolvedPath, JSON.stringify(valueContent, null, 2));
+        if (!lodash.isEqual(target.keys, ['locales'])) {
+          const localeIndexPath = resolvePaths([__dirname, '../src', ...target.keys, valueKey, 'locale', 'index.ts']);
+          const project = new Project();
+          const sourceFile = project.addSourceFileAtPath(localeIndexPath);
+          sourceFile.addImportDeclaration({
+            defaultImport: lanugage,
+            moduleSpecifier: `./${lanugage}.json`,
+          });
+          const callExpressions = sourceFile.getDescendantsOfKind(SyntaxKind.CallExpression);
+          const targetCallExpression = callExpressions.find((callExpression) => {
+            const expression = callExpression.getExpression();
+            return expression.getText() === 'i18next.addResourceBundle';
+          });
+          if (targetCallExpression) {
+            const args = targetCallExpression.getArguments();
+            const argValues = args.map((arg) => arg.getText());
+            const i18nNs = argValues[1];
+            sourceFile.addStatements(`i18next.addResourceBundle('${lanugage}', ${i18nNs}, ${lanugage});`);
+          } else {
+            console.log('未找到 i18next.addResourceBundle 方法调用');
+          }
+          sourceFile.saveSync();
+        }
+      });
+    }
+  });
+} else {
+  console.log('请传入语言参数, 比如 npx ts-node generate_each_locales.ts zh_CN');
+}
+export {};
+
+// Path: scripts/generate_each_locales.js
