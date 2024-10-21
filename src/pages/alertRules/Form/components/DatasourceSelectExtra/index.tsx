fc9691d67a54798e44724e20623908edf1f5a77e
jsers <xqing9024@gmail.com>
2024-10-21 17:48:19 +0800

    code refactor

--- src/pages/alertRules/Form/components/DatasourceSelectExtra/index.tsx
+++ src/pages/alertRules/Form/components/DatasourceSelectExtra/index.tsx
@@ -1,29 +1,19 @@
 import React from 'react';
 import { Form } from 'antd';
 import _ from 'lodash';
-import { DatasourceCateEnum, IS_PLUS } from '@/utils/constant';
+import { IS_PLUS } from '@/utils/constant';
+
 // @ts-ignore
-import * as MySQLMeta from 'plus:/datasource/mysql/components/Meta';
-// @ts-ignore
-import * as CKMeta from 'plus:/datasource/clickHouse/components/Meta';
-// @ts-ignore
-import * as Meta from 'plus:/components/Meta';
+import DatasourceSelectExtra from 'plus:/components/DatasourceSelectExtra';
 
 export default function index() {
   const datasourceCate = Form.useWatch('cate');
   const datasourceValue = Form.useWatch('datasource_ids');
+  const curDatasourceValue = _.isArray(datasourceValue) ? _.head(datasourceValue) : datasourceValue;
 
-  if (datasourceCate === DatasourceCateEnum.mysql && datasourceValue !== undefined) {
-    const realDatasourceValue = _.isArray(datasourceValue) ? _.head(datasourceValue) : datasourceValue;
-    return <MySQLMeta.MetaModal datasourceValue={realDatasourceValue} />;
+  if (IS_PLUS) {
+    return <DatasourceSelectExtra datasourceCate={datasourceCate} datasourceValue={curDatasourceValue} />;
   }
-  if (datasourceCate === DatasourceCateEnum.ck && datasourceValue !== undefined) {
-    const realDatasourceValue = _.isArray(datasourceValue) ? _.head(datasourceValue) : datasourceValue;
-    return <CKMeta.MetaModal datasourceValue={realDatasourceValue} />;
-  }
-  if (datasourceCate === DatasourceCateEnum.influxDB && datasourceValue !== undefined) {
-    const realDatasourceValue = _.isArray(datasourceValue) ? _.head(datasourceValue) : datasourceValue;
-    return <Meta.MetaModal datasourceCate={DatasourceCateEnum.influxDB} datasourceValue={realDatasourceValue} />;
-  }
+
   return null;
 }
