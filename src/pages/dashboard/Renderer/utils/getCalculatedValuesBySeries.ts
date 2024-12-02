ccb0f0dfa48a6670e6759c0799c3632084481cce
jsers <xqing9024@gmail.com>
2024-12-02 20:37:04 +0800

    code refactor

--- src/pages/dashboard/Renderer/utils/getCalculatedValuesBySeries.ts
+++ src/pages/dashboard/Renderer/utils/getCalculatedValuesBySeries.ts
@@ -210,7 +210,7 @@ const getCalculatedValuesBySeries = (
 
 export const getLegendValues = (series: any[], standardOptions, hexPalette: string[], stack = false, valueMappings?: IValueMapping[], overrides?: IOverride[]) => {
   let { decimals, dateFormat } = standardOptions || {};
-  let unit = standardOptions.unit || standardOptions.util; // TODO: 兼容之前写错的 util
+  let unit = standardOptions?.unit || standardOptions?.util; // TODO: 兼容之前写错的 util
   const newSeries = stack ? _.reverse(_.clone(series)) : series;
   const values = _.map(newSeries, (serie, idx) => {
     const override = _.find(overrides, (item) => item.matcher.value === serie.refId);
