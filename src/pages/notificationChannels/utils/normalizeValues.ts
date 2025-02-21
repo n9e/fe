d30fea58a6d5b3bd8cc9f7c6eb8b421200869448
jsers <xqing9024@gmail.com>
2025-02-21 15:40:47 +0800

    code refactor

--- src/pages/notificationChannels/utils/normalizeValues.ts
+++ src/pages/notificationChannels/utils/normalizeValues.ts
@@ -8,11 +8,11 @@ export function normalizeFormValues(values: ChannelItem): any {
     http_request_config: {
       ...values.http_request_config,
       // 将 headers: {key: string, value: string}[] 转换成 {[key: string]: string}
-      headers: _.fromPairs(_.map(values.http_request_config.headers, (item) => [item.key, item.value])),
+      headers: _.fromPairs(_.map(values.http_request_config?.headers, (item) => [item.key, item.value])),
       request: {
-        ...(values.http_request_config.request || {}),
+        ...(values.http_request_config?.request ?? {}),
         // 将 parameters: {key: string, value: string}[] 转换成 {[key: string]: string}
-        parameters: _.fromPairs(_.map(values.http_request_config.request?.parameters, (item) => [item.key, item.value])),
+        parameters: _.fromPairs(_.map(values.http_request_config?.request?.parameters, (item) => [item.key, item.value])),
       },
     },
   };
@@ -24,16 +24,16 @@ export function normalizeInitialValues(values: any): ChannelItem {
     http_request_config: {
       ...values.http_request_config,
       // 将 headers: {[key: string]: string} 转换成 {key: string, value: string}[]
-      headers: _.map(values.http_request_config.headers, (value, key) => {
+      headers: _.map(values.http_request_config?.headers, (value, key) => {
         return {
           key,
           value,
         };
       }),
       request: {
-        ...(values.http_request_config.request || {}),
+        ...(values.http_request_config?.request ?? {}),
         // 将 parameters: {[key: string]: string} 转换成 {key: string, value: string}[]
-        parameters: _.map(values.http_request_config.request?.parameters, (value, key) => {
+        parameters: _.map(values.http_request_config?.request?.parameters, (value, key) => {
           return {
             key,
             value,
