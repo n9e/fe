faf5cf2119d0ffd2c9891504f734343c594fb776
jsers <xqing9024@gmail.com>
2025-03-05 20:19:46 +0800

    fix: time_range value

--- src/pages/notificationRules/utils/normalizeValues.ts
+++ src/pages/notificationRules/utils/normalizeValues.ts
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import moment from 'moment';
 
 import { RuleItem } from '../types';
 
@@ -8,8 +9,13 @@ export function normalizeFormValues(values: RuleItem): any {
     notify_configs: _.map(values.notify_configs, (item) => {
       return {
         ...item,
-        // 将 params: {key: string, value: string}[] 转换成 {[key: string]: string}
-        params: _.fromPairs(_.map(item.params, (item) => [item.key, item.value])),
+        time_ranges: _.map(item.time_ranges, (timeRange) => {
+          return {
+            ...timeRange,
+            start: timeRange.start.format('HH:mm'),
+            end: timeRange.end.format('HH:mm'),
+          };
+        }),
       };
     }),
   };
@@ -21,11 +27,11 @@ export function normalizeInitialValues(values: any): RuleItem {
     notify_configs: _.map(values.notify_configs, (item) => {
       return {
         ...item,
-        // 将 params: {[key: string]: string} 转换成 {key: string, value: string}[]
-        params: _.map(item.params, (value, key) => {
+        time_ranges: _.map(item.time_ranges, (timeRange) => {
           return {
-            key,
-            value,
+            ...timeRange,
+            start: moment(timeRange.start, 'HH:mm'),
+            end: moment(timeRange.end, 'HH:mm'),
           };
         }),
       };
