760f827671be47b19ca40ac94158071ffd87b447
ngyixuan <ngyixuan2009@gmail.com>
2025-04-24 16:45:05 +0800

    feat: replace header text to icon

---
+++ src/components/pageLayout/icons/LanguageIcon.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const LanguageIcon: React.FC = () => (
+  <svg
+    xmlns='http://www.w3.org/2000/svg'
+    width='12'
+    viewBox='0 0 24 24'
+    fill='none'
+    stroke='#333'
+    strokeWidth='2'
+    strokeLinecap='round'
+    strokeLinejoin='round'
+    style={{ opacity: 1 }}
+  >
+    <path d='m5 8 6 6'></path>
+    <path d='m4 14 6-6 3-3'></path>
+    <path d='M2 5h12'></path>
+    <path d='M7 2h1'></path>
+    <path d='m22 22-5-10-5 10'></path>
+    <path d='M14 18h6'></path>
+  </svg>
+);
+
+export default LanguageIcon;
