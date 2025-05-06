f45b908c40bbe933cd276df365b2fd42ef4b88b6
jsers <xqing9024@gmail.com>
2025-05-06 10:52:36 +0800

    refactor: change menu config

--- src/components/SideMenu/menu.tsx
+++ src/components/SideMenu/menu.tsx
@@ -101,6 +101,10 @@ export const getMenuList = () => {
           label: 'menu.notification_templates',
         },
         {
+          key: '/event-pipeline',
+          label: 'menu.event_pipeline',
+        },
+        {
           key: '/help/notification-settings',
           label: 'menu.notification_settings',
           deprecated: true,
