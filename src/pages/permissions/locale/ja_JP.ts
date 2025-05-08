5c6b2abdc256c45c5e9e05c462bdbc1fdddaf08e
jsers <xqing9024@gmail.com>
2025-05-08 10:40:53 +0800

    code refactor

--- src/pages/permissions/locale/ja_JP.ts
+++ src/pages/permissions/locale/ja_JP.ts
@@ -1,10 +1,10 @@
 const ja_JP = {
-  title: "権限管理",
-  roles: "ロールリスト",
-  role_add: "ロールの追加",
+  title: 'ロール管理',
+  roles: 'ロールリスト',
+  role_add: 'ロールの追加',
   edit_title: {
-    post: "ロールの追加",
-    put: "ロールの編集",
+    post: 'ロールの追加',
+    put: 'ロールの編集',
   },
 };
 
