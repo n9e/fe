41ff334d415c2e46d088f20317d0c4f73e18379b
jsers <xqing9024@gmail.com>
2025-05-27 11:06:08 +0800

    refactor: update usePagination

--- src/components/usePagination.tsx
+++ src/components/usePagination.tsx
@@ -1,6 +1,8 @@
-import { CommonStateContext } from '@/App';
 import React, { useContext } from 'react';
 import { useTranslation } from 'react-i18next';
+
+import { CommonStateContext } from '@/App';
+import { IS_ENT } from '@/utils/constant';
 
 interface PaginationProps {
   PAGESIZE_KEY?: string; // @deprecated Use pageSizeLocalstorageKey instead
@@ -12,9 +14,12 @@ interface PaginationProps {
 
 type TablePaginationPosition = 'topLeft' | 'topCenter' | 'topRight' | 'bottomLeft' | 'bottomCenter' | 'bottomRight';
 
+const DEFAULT_PAGESIZE_OPTIONS = IS_ENT ? [10, 15, 20, 50, 100] : [10, 20, 50, 100];
+const DEFAULT_PAGESIZE = IS_ENT ? 15 : 10;
+
 export default function usePagination(props: PaginationProps) {
   const { tablePaginationPosition } = useContext(CommonStateContext);
-  const { PAGESIZE_KEY, pageSizeLocalstorageKey, defaultPageSize = 10, pageSizeOptions = ['10', '20', '50', '100'], showSizeChanger = true } = props;
+  const { PAGESIZE_KEY, pageSizeLocalstorageKey, defaultPageSize = DEFAULT_PAGESIZE, pageSizeOptions = DEFAULT_PAGESIZE_OPTIONS, showSizeChanger = true } = props;
   const curPageSizeLocalstorageKey = pageSizeLocalstorageKey || PAGESIZE_KEY;
   const pageSizeLocalValue = curPageSizeLocalstorageKey ? localStorage.getItem(curPageSizeLocalstorageKey) : null;
   const [pageSize, setPageSize] = React.useState<number>(pageSizeLocalValue ? Number(pageSizeLocalValue) : defaultPageSize);
