15000dde04d155db8cf68e6ba63415b517af32e0
jsers <xqing9024@gmail.com>
2025-06-24 22:33:39 +0800

    refactor: replace fixed maxDataPoints with panelWidth in step calculation

--- src/pages/dashboard/Editor/QueryEditor/Prometheus.tsx
+++ src/pages/dashboard/Editor/QueryEditor/Prometheus.tsx
@@ -151,13 +151,13 @@ export default function Prometheus({ variableConfig, time, datasourceValue }) {
                         </Form.Item>
                       </Col>
                       <Col flex='120px'>
-                        <Form.Item label='Max data points' tooltip={t('query.prometheus.maxDataPoints.tip')} {...field} name={[field.name, 'maxDataPoints']} initialValue={240}>
+                        <Form.Item label='Max data points' tooltip={t('query.prometheus.maxDataPoints.tip')} {...field} name={[field.name, 'maxDataPoints']}>
                           <InputNumber style={{ width: '100%' }} placeholder='240' min={1} />
                         </Form.Item>
                       </Col>
                       <Col flex='72px'>
                         <Form.Item label='Min step' tooltip={t('query.prometheus.minStep.tip')} {...field} name={[field.name, 'step']}>
-                          <Resolution />
+                          <Resolution placeholder='15' />
                         </Form.Item>
                       </Col>
                       <Col flex='72px'>
