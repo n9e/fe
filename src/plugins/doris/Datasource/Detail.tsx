633f65bf9aef8694c791e56ffdf03555a404b552
jsers <xqing9024@gmail.com>
2025-06-25 16:33:10 +0800

    code refactor

--- src/plugins/doris/Datasource/Detail.tsx
+++ src/plugins/doris/Datasource/Detail.tsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Button, Row, Col } from 'antd';
+import React from 'react';
+import { Row, Col } from 'antd';
 import _ from 'lodash';
 import { useTranslation } from 'react-i18next';
 interface Props {
@@ -8,6 +8,8 @@ interface Props {
 export default function Index(props: Props) {
   const { t } = useTranslation('datasourceManage');
   const { data } = props;
+  const type = 'doris';
+
   return (
     <div>
       <div className='page-title'>FE node</div>
@@ -32,12 +34,35 @@ export default function Index(props: Props) {
           </Col>
         </Row>
       </div>
+      <div className='page-title'>{t('common:advanced_settings')}</div>
+      <div className='flash-cat-block'>
+        <Col span={24}>{t('datasource:datasource.timeout')}</Col>
+        <Col span={24} className='second-color'>
+          {data.settings[`${type}.timeout`] || '-'}
+        </Col>
+        <Col span={24}>{t('datasource:datasource.max_query_rows')}</Col>
+        <Col span={24} className='second-color'>
+          {data.settings[`${type}.max_query_rows`] || '-'}
+        </Col>
+        <Col span={24}>{t('datasource:datasource.max_idle_conns')}</Col>
+        <Col span={24} className='second-color'>
+          {data.settings[`${type}.max_idle_conns`] || '-'}
+        </Col>
+        <Col span={24}>{t('datasource:datasource.max_open_conns')}</Col>
+        <Col span={24} className='second-color'>
+          {data.settings[`${type}.max_open_conns`] || '-'}
+        </Col>
+        <Col span={24}>{t('datasource:datasource.conn_max_lifetime')}</Col>
+        <Col span={24} className='second-color'>
+          {data.settings[`${type}.conn_max_lifetime`] || '-'}
+        </Col>
+      </div>
       <div className='page-title'>{t('form.other')}</div>
       <div className='flash-cat-block'>
         <Row gutter={16}>
           <Col span={24}>{t('datasourceManage:form.cluster')}：</Col>
           <Col span={24} className='second-color'>
-            {data.settings[`doris.cluster_name`] || '-'}
+            {data['cluster_name'] || '-'}
           </Col>
         </Row>
       </div>
