9f7605ec62cd1cec8edd3fa39f6294bdce34c94b
jsers <xqing9024@gmail.com>
2025-08-15 11:56:55 +0800

    feat: DatasourceSelect V3

---
+++ src/components/DatasourceSelect/DatasourceSelectV3/index.tsx
@@ -0,0 +1,112 @@
+import React, { useState, useEffect } from 'react';
+import { Select, Space, Tag, Spin } from 'antd';
+import { SelectProps } from 'antd/lib/select';
+import _ from 'lodash';
+import { useDebounceFn } from 'ahooks';
+
+import { getDatasourceBriefList, DatasourceItem } from '@/services/common';
+import { Cate } from '@/components/AdvancedWrap/utils';
+
+interface Props {
+  datasourceCateList: Cate[];
+  ajustDatasourceList?: (list: DatasourceItem[]) => DatasourceItem[];
+  onChange?: (value: string | number, datasourceCate: string) => void;
+  onClear?: () => void;
+}
+
+export default function index(props: SelectProps & Props) {
+  const { datasourceCateList, ajustDatasourceList, onChange, onClear } = props;
+  const [fetching, setFetching] = useState(false);
+  const allDatasourceListRef = React.useRef<DatasourceItem[]>([]);
+  const [datasourceList, setDatasourceList] = useState<DatasourceItem[]>([]);
+  const fetcher = (query?: string) => {
+    return getDatasourceBriefList(query)
+      .then((list) => {
+        let newList = list;
+        if (ajustDatasourceList) {
+          newList = ajustDatasourceList(list);
+        }
+        if (!query) {
+          allDatasourceListRef.current = newList;
+        }
+        setDatasourceList(newList);
+      })
+      .catch(() => {
+        setDatasourceList([]);
+      });
+  };
+
+  const { run: debounceFetcher } = useDebounceFn(
+    (value: string) => {
+      setFetching(true);
+      fetcher(value).finally(() => {
+        setFetching(false);
+      });
+    },
+    { wait: 500 },
+  );
+
+  useEffect(() => {
+    fetcher();
+  }, []);
+
+  return (
+    <Select
+      dropdownMatchSelectWidth={false}
+      {..._.omit(props, ['datasourceCateList', 'ajustDatasourceList'])}
+      showSearch
+      filterOption={false}
+      onSearch={debounceFetcher}
+      notFoundContent={fetching ? <Spin size='small' /> : null}
+      optionLabelProp='optionLabel'
+      options={_.map(_.orderBy(datasourceList, ['is_default', 'plugin_type'], ['desc', 'asc']), (item) => {
+        const datasourceCate = _.find(datasourceCateList, { value: item.plugin_type });
+        return {
+          originLabel: item.name,
+          optionLabel: (
+            <div>
+              <Space>
+                <img src={datasourceCate?.logo} alt={datasourceCate?.label} height={16} />
+                {item.name}
+              </Space>
+            </div>
+          ),
+          label: (
+            <div className='flex items-center gap-2 justify-between'>
+              <Space>
+                <img src={datasourceCate?.logo} alt={datasourceCate?.label} height={16} />
+                {item.name}
+                {item.is_default && <Tag color='var(--fc-fill-primary)'>default</Tag>}
+              </Space>
+              <span
+                style={{
+                  color: 'var(--fc-text-4)',
+                }}
+              >
+                {datasourceCate?.label}
+              </span>
+            </div>
+          ),
+          value: item.id,
+        };
+      })}
+      onChange={(value) => {
+        if (onChange) {
+          const curCate = _.find(datasourceList, { id: value })?.plugin_type;
+          if (!curCate) {
+            return;
+          }
+          onChange(value, curCate);
+        }
+      }}
+      onClear={onClear}
+      allowClear={!!onClear}
+      onDropdownVisibleChange={(visible) => {
+        if (!visible) {
+          // 关闭下拉时重置数据源列表
+          setDatasourceList(allDatasourceListRef.current);
+        }
+      }}
+    />
+  );
+}
--- src/components/DatasourceSelect/index.tsx
+++ src/components/DatasourceSelect/index.tsx
@@ -5,6 +5,7 @@ import ProdSelect from './ProdSelect';
 import DatasourceSelect from './DatasourceSelect';
 import DatasourceCateSelectV2 from './DatasourceCateSelectV2';
 import DatasourceSelectV2 from './DatasourceSelectV2';
+import DatasourceSelectV3 from './DatasourceSelectV3';
 
 import './locale';
-export { DatasourceCateSelect, ProSvg, ProdSelect, DatasourceSelect, DatasourceCateSelectV2, DatasourceSelectV2 };
+export { DatasourceCateSelect, ProSvg, ProdSelect, DatasourceSelect, DatasourceCateSelectV2, DatasourceSelectV2, DatasourceSelectV3 };
