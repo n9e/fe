0763c19c62e8da631781c2b2ed7b2236c6884d13
jsers <xqing9024@gmail.com>
2025-09-23 15:32:47 +0800

    feat: add start and end params

--- src/pages/historyEvents/index.tsx
+++ src/pages/historyEvents/index.tsx
@@ -8,7 +8,7 @@ import { AlertOutlined } from '@ant-design/icons';
 import { useTranslation } from 'react-i18next';
 
 import PageLayout, { HelpLink } from '@/components/pageLayout';
-import { parseRange, getDefaultValue } from '@/components/TimeRangePicker';
+import { parseRange, getDefaultValue, isMathString, IRawTimeRange } from '@/components/TimeRangePicker';
 
 import { getEvents, getEventsByIds } from './services';
 import '../event/index.less';
@@ -17,11 +17,20 @@ import './locale';
 import ListNG, { CACHE_KEY } from './ListNG';
 
 const getFilter = (query) => {
+  let defaultRange: IRawTimeRange | undefined = undefined;
+  if (query.start && query.end) {
+    if (isMathString(query.start) && isMathString(query.end)) {
+      defaultRange = { start: query.start, end: query.end };
+    } else if (!_.isNaN(_.toNumber(query.start)) && !_.isNaN(_.toNumber(query.end))) {
+      const startMoment = moment.unix(query.start);
+      const endMoment = moment.unix(query.end);
+      if (startMoment.isValid() && endMoment.isValid()) {
+        defaultRange = { start: startMoment, end: endMoment };
+      }
+    }
+  }
   return {
-    range: getDefaultValue(CACHE_KEY, {
-      start: 'now-6h',
-      end: 'now',
-    }),
+    range: defaultRange ?? getDefaultValue(CACHE_KEY, { start: 'now-6h', end: 'now' }),
     datasource_ids: query.datasource_ids ? _.split(query.datasource_ids, ',').map(Number) : [],
     bgid: query.bgid ? Number(query.bgid) : undefined,
     severity: query.severity ? Number(query.severity) : undefined,
