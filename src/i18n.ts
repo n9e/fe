5a7fbab2767e27b6e90d0480115720036d2500ae
star-6 <290043518@qq.com>
2025-10-31 10:19:44 +0800

    update comb_feats.js

--- src/i18n.ts
+++ src/i18n.ts
@@ -18,8 +18,13 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import ICU from 'i18next-icu';
 import _ from 'lodash';
-import { withTolgee, Tolgee, I18nextPlugin } from '@tolgee/i18next';
-import { InContextTools } from '@tolgee/web/tools';
+import { withTolgee, Tolgee, I18nextPlugin, DevTools } from '@tolgee/i18next';
+import { InContextTools, ContextUi } from '@tolgee/web/tools';
+
+const urlParams = new URLSearchParams(window.location.search);
+const enableTolgeeEdit = urlParams.get('tolgee') === 'edit';
+
+console.log('Tolgee edit mode:', enableTolgeeEdit, window.location);
 
 // const languages = ['zh_CN', 'en_US', 'zh_HK', 'ru_RU', 'ja_JP'];
 const languages = ['zh_CN', 'en_US', 'zh_HK'];
@@ -29,7 +34,7 @@ if (localStorageLanguage && _.includes(languages, localStorageLanguage)) {
   language = localStorageLanguage;
 }
 
-function getTranslations() {
+function getTolgeeTranslations() {
   const translations: any = import.meta.glob('../**/{locale,locales}/index.(ts|js)', { eager: true });
   const result = {};
 
@@ -43,17 +48,33 @@ function getTranslations() {
   return result;
 }
 
+function getI18nextTranslations() {
+  const translations: any = import.meta.glob('../**/{locale,locales}/index.(ts|js)', { eager: true });
+  const result = { en_US: {}, zh_CN: {}, zh_HK: {} };
+
+  for (const path in translations) {
+    const module = translations[path]?.default;
+    languages?.forEach((language) => {
+      result[language][module?.namespace] = module?.languages[language];
+    });
+  }
+  // console.log('getI18nextTranslations', result);
+  return result;
+}
+
 const API_URL = import.meta.env.VITE_TOLGEE_API_URL;
 const API_KEY = import.meta.env.VITE_TOLGEE_API_KEY;
-const staticData = getTranslations();
+const staticData = getTolgeeTranslations();
 
-console.log('API_URL', API_URL);
-console.log('API_KEY', API_KEY);
+// console.log('API_URL', API_URL);
+// console.log('API_KEY', API_KEY);
 
 let tolgee;
+
 if (!!API_URL && !!API_KEY) {
   tolgee = Tolgee()
     .use(InContextTools()) // 可以在页面直接改翻译
+    // .use(DevTools())
     .use(I18nextPlugin()) // 兼容 react-i18next
     .init({
       // for development
@@ -64,7 +85,6 @@ if (!!API_URL && !!API_KEY) {
       staticData,
       defaultNs: 'translation',
       ns: ['translation', 'common', 'datasource'],
-      fallbackNs: ['translation', 'common', 'datasource'],
     });
 } else {
   tolgee = Tolgee()
@@ -73,11 +93,11 @@ if (!!API_URL && !!API_KEY) {
       staticData,
       defaultNs: 'translation',
       ns: ['translation', 'common', 'datasource'],
-      fallbackNs: ['translation', 'common', 'datasource'],
     });
 }
 
 const i18nInit = withTolgee(i18n, tolgee).use(ICU).use(initReactI18next);
+
 i18nInit.init({
   lng: language,
   interpolation: {
@@ -88,4 +108,5 @@ i18nInit.init({
     useSuspense: false,
   },
 });
+
 export { i18nInit, tolgee };
