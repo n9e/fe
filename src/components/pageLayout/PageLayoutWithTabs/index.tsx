95528c92df02a5d38302a0966403893232f5da21
jsers <xqing9024@gmail.com>
2025-11-06 10:17:48 +0800

    refactor: add container span for introIcon styling

--- src/components/pageLayout/PageLayoutWithTabs/index.tsx
+++ src/components/pageLayout/PageLayoutWithTabs/index.tsx
@@ -174,7 +174,7 @@ const PageLayout: React.FC<IPageLayoutProps> = ({ icon, title, rightArea, introI
                 </div>
 
                 <div className={'page-header-right-area'} style={{ display: sessionStorage.getItem('menuHide') === '1' ? 'none' : undefined }}>
-                  {introIcon}
+                  <span className='page-layout-intro-container'>{introIcon}</span>
                   {docFn && (
                     <a onClick={() => docFn()} className='page-layout-doc-container'>
                       {t('docs')}
