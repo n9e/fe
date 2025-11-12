f345b409eb5b29bcfc4cdb175931f7bb8082d430
jsers <xqing9024@gmail.com>
2025-11-12 15:56:55 +0800

    code refactor

--- src/plugins/doris/globalState.ts
+++ src/plugins/doris/globalState.ts
@@ -1,8 +1,6 @@
 import { createGlobalState } from 'react-hooks-global-state';
 import moment from 'moment';
 
-import { FieldConfigVersion2 } from '@/pages/log/IndexPatterns/types';
-
 export const { useGlobalState, getGlobalState } = createGlobalState<{
   mySQLTableFields: string[];
   explorerParsedRange: {
@@ -10,10 +8,8 @@ export const { useGlobalState, getGlobalState } = createGlobalState<{
     end?: moment.Moment;
   };
   explorerSnapRange: { start?: number; end?: number }; // unix timestamp
-  fieldConfig?: FieldConfigVersion2;
 }>({
   mySQLTableFields: [],
   explorerParsedRange: { start: undefined, end: undefined },
   explorerSnapRange: { start: undefined, end: undefined },
-  fieldConfig: undefined,
 });
