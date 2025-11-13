3d1fb4871ae427a5600cb661a51350c981a6ffb2
jsers <xqing9024@gmail.com>
2025-11-13 17:34:06 +0800

    feat: add DingTalk SSO

--- src/pages/help/SSOConfigs/locale/en_US.ts
+++ src/pages/help/SSOConfigs/locale/en_US.ts
@@ -1,4 +1,9 @@
 const en_US = {
   title: 'Single Sign-On settings',
+  LDAP: 'LDAP',
+  CAS: 'CAS',
+  OIDC: 'OIDC',
+  OAuth2: 'OAuth2',
+  dingtalk: 'DingTalk',
 };
 export default en_US;
--- src/pages/help/SSOConfigs/locale/ru_RU.ts
+++ src/pages/help/SSOConfigs/locale/ru_RU.ts
@@ -1,5 +1,10 @@
 const ru_RU = {
   title: 'Управление SSO',
+  LDAP: 'LDAP',
+  CAS: 'CAS',
+  OIDC: 'OIDC',
+  OAuth2: 'OAuth2',
+  dingtalk: 'DingTalk',
 };
 
 export default ru_RU;
