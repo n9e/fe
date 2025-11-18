407f68fa8a14a6b4493a54921f39663422149613
jsers <xqing9024@gmail.com>
2025-11-18 15:52:29 +0800

    feat: add tooltip i18n

--- src/pages/alertRules/locale/ja_JP.ts
+++ src/pages/alertRules/locale/ja_JP.ts
@@ -31,6 +31,7 @@ const ja_JP = {
   effective_time_end: '終了時間',
   effective_time_end_msg: '終了時間は空できません',
   effective_time_week_msg: '有効期間を選択してください',
+  effective_time_tip: '00:00-00:00 と 00:00-23:59 はどちらも終日有効を意味します',
   enable_in_bg: 'このビジネスグループでのみ有効',
   enable_in_bg_tip:
     'アラートイベントの ident ラベルに基づいて所属関係を判断し、つまり：アラートイベントに ident ラベルがあり、その ident に対応するマシンがこのビジネスグループに属していない場合、このアラートイベントは破棄されます',
