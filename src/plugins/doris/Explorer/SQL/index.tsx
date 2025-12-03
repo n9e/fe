3aed91804ddc96b1d799bb99f6f5327af98ba680
jsers <xqing9024@gmail.com>
2025-12-03 20:50:07 +0800

    refactor: doris explorer

--- src/plugins/doris/Explorer/SQL/index.tsx
+++ src/plugins/doris/Explorer/SQL/index.tsx
@@ -1,16 +1,18 @@
 import React, { useState, useContext } from 'react';
 import { Resizable } from 're-resizable';
-import { useTranslation } from 'react-i18next';
-import { Button, Form } from 'antd';
-import { InfoCircleOutlined } from '@ant-design/icons';
+import { useTranslation, Trans } from 'react-i18next';
+import { Alert, Button, Form, message, Modal, Space } from 'antd';
+import { InfoCircleOutlined, CopyOutlined } from '@ant-design/icons';
 import _ from 'lodash';
 
 import { CommonStateContext } from '@/App';
 import { DatasourceCateEnum } from '@/utils/constant';
+import { copy2ClipBoard } from '@/utils';
 import Meta from '@/components/Meta';
 import DocumentDrawer from '@/components/DocumentDrawer';
 
-import { NAME_SPACE, SQL_SIDEBAR_CACHE_KEY } from '../../constants';
+import { NAME_SPACE, SQL_SIDEBAR_CACHE_KEY, DATE_TYPE_LIST } from '../../constants';
+import { getDorisIndex } from '../../services';
 import QueryBuilder from './QueryBuilder';
 import Content from './Content';
 
@@ -28,6 +30,16 @@ export default function index(props: Props) {
   const { submode, disabled, datasourceValue, executeQuery } = props;
   const [width, setWidth] = useState(_.toNumber(localStorage.getItem(SQL_SIDEBAR_CACHE_KEY) || 200));
   const [executeLoading, setExecuteLoading] = useState(false);
+  const [queryWarnModalVisible, setQueryWarnModalVisible] = useState(false);
+  const handleExecuteQuery = () => {
+    const queryValue = form.getFieldValue(['query', 'query']);
+    // 如果 queryValue 里未包含关键字：$__time 或 $__unixEpoch，触发查询时阻断弹窗
+    if (!queryValue.includes('$__time') && !queryValue.includes('$__unixEpoch')) {
+      setQueryWarnModalVisible(true);
+      return;
+    }
+    executeQuery();
+  };
 
   return (
     <div className='explorer-query-container'>
@@ -51,11 +63,30 @@ export default function index(props: Props) {
             datasourceValue={datasourceValue}
             onTreeNodeClick={(nodeData) => {
               const query = form.getFieldValue(['query']);
-              _.set(query, 'query', `select * from ${nodeData.database}.${nodeData.table} limit 20;`);
-              form.setFieldsValue({
-                query,
-              });
-              executeQuery();
+
+              getDorisIndex({ cate: DatasourceCateEnum.doris, datasource_id: datasourceValue, database: nodeData.database, table: nodeData.table })
+                .then((res) => {
+                  let dateField = 'timestamp';
+                  const firstDateField = _.find(res, (item) => {
+                    return _.includes(DATE_TYPE_LIST, item.type.toLowerCase());
+                  })?.field;
+                  if (firstDateField) {
+                    dateField = firstDateField;
+                  }
+                  _.set(query, 'query', `select * from ${nodeData.database}.${nodeData.table} WHERE $__timeFilter(${dateField}) limit 20;`);
+                  form.setFieldsValue({
+                    query,
+                  });
+                  handleExecuteQuery();
+                })
+                .catch(() => {
+                  message.warning(t('query.get_index_fail'));
+                  _.set(query, 'query', `select * from ${nodeData.database}.${nodeData.table} WHERE $__timeFilter(timestamp) limit 20;`);
+                  form.setFieldsValue({
+                    query,
+                  });
+                  handleExecuteQuery();
+                });
             }}
           />
         </Resizable>
@@ -68,11 +99,11 @@ export default function index(props: Props) {
       >
         <QueryBuilder
           extra={
-            <Button type='primary' onClick={executeQuery} disabled={disabled} loading={executeLoading}>
+            <Button type='primary' onClick={handleExecuteQuery} disabled={disabled} loading={executeLoading}>
               {t('query.execute')}
             </Button>
           }
-          executeQuery={executeQuery}
+          executeQuery={handleExecuteQuery}
           datasourceValue={datasourceValue}
           labelInfo={
             <InfoCircleOutlined
@@ -90,6 +121,94 @@ export default function index(props: Props) {
         />
         <Content submode={submode} setExecuteLoading={setExecuteLoading} />
       </div>
+      <Modal
+        width={700}
+        visible={queryWarnModalVisible}
+        footer={[
+          <Button
+            key='ok'
+            type='primary'
+            onClick={() => {
+              setQueryWarnModalVisible(false);
+              executeQuery();
+            }}
+          >
+            {t('query.warn_message_btn_1')}
+          </Button>,
+          <Button
+            key='cancel'
+            onClick={() => {
+              setQueryWarnModalVisible(false);
+            }}
+          >
+            {t('query.warn_message_btn_2')}
+          </Button>,
+        ]}
+        onCancel={() => setQueryWarnModalVisible(false)}
+      >
+        <Alert className='mt-4 mb-4' type='warning' showIcon message={t('query.warn_message')} />
+        <div className='mb-2'>{t('query.warn_message_content_1')}</div>
+        <div className='mb-2'>{t('query.warn_message_content_2')}</div>
+        <div className='mb-2'>
+          <div>
+            <Space>
+              <code>{`$__timeFilter(dateColumn)`}</code>
+              <CopyOutlined
+                onClick={() => {
+                  copy2ClipBoard(`$__timeFilter(dateColumn)`);
+                }}
+              />
+            </Space>
+          </div>
+          <div>
+            <Space>
+              <code>{`$__unixEpochFilter(dateColumn) `}</code>
+              <CopyOutlined
+                onClick={() => {
+                  copy2ClipBoard(`$__unixEpochFilter(dateColumn) `);
+                }}
+              />
+            </Space>
+          </div>
+          <div>
+            <Space>
+              <code>{`$__unixEpochNanoFilter(dateColumn)`}</code>
+              <CopyOutlined
+                onClick={() => {
+                  copy2ClipBoard(`$__unixEpochNanoFilter(dateColumn)`);
+                }}
+              />
+            </Space>
+          </div>
+        </div>
+        <div className='mb-2'>
+          {t('query.warn_message_content_3')}
+          <Space>
+            <code>
+              {`SELECT count(*) as count FROM db_name.table_name `}
+              <span
+                style={{
+                  color: 'var(--fc-orange-5-color)',
+                }}
+              >{`WHERE $__timeFilter(timestamp) `}</span>
+            </code>
+            <CopyOutlined
+              onClick={() => {
+                copy2ClipBoard(`SELECT count(*) as count FROM db_name.table_name WHERE $__timeFilter(timestamp)`);
+              }}
+            />
+          </Space>
+        </div>
+        <div>
+          <Trans
+            ns={NAME_SPACE}
+            i18nKey='query.warn_message_content_4'
+            components={{
+              a: <a target='__blank' href='/docs/content/flashcat/log/discover/what-is-sql-mode-in-doris-discover/' />,
+            }}
+          />
+        </div>
+      </Modal>
     </div>
   );
 }
