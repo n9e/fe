ddbeb65a8dfca4992ac08e5964712e8ced44fd83
jsers <xqing9024@gmail.com>
2025-12-18 20:44:13 +0800

    code refactor

--- src/plugins/doris/Explorer/SQL/Raw.tsx
+++ src/plugins/doris/Explorer/SQL/Raw.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import { Empty, Form, Space, Pagination } from 'antd';
 import _ from 'lodash';
 import moment from 'moment';
 import { DatasourceCateEnum } from '@/utils/constant';
-import { useRequest } from 'ahooks';
+import { useRequest, useGetState } from 'ahooks';
 import { useTranslation } from 'react-i18next';
 
 import { IS_PLUS } from '@/utils/constant';
@@ -11,6 +11,7 @@ import { parseRange } from '@/components/TimeRangePicker';
 import flatten from '@/pages/explorer/components/LogsViewer/utils/flatten';
 import getFieldsFromTableData from '@/pages/explorer/components/LogsViewer/utils/getFieldsFromTableData';
 import LogsViewer from '@/pages/explorer/components/LogsViewer';
+import { useGlobalState } from '@/pages/explorer/globalState';
 
 import { NAME_SPACE, SQL_LOGS_OPTIONS_CACHE_KEY } from '../../constants';
 import { logQuery } from '../../services';
@@ -25,24 +26,36 @@ interface IProps {
 
 function Raw(props: IProps) {
   const { t } = useTranslation(NAME_SPACE);
+  const [tabKey] = useGlobalState('tabKey');
+  const logsTableSelectors = `.explorer-container-${tabKey} .n9e-event-logs-table .ant-table-body`;
   const form = Form.useFormInstance();
   const refreshFlag = Form.useWatch('refreshFlag');
   const datasourceValue = Form.useWatch(['datasourceValue']);
   const queryValues = Form.useWatch(['query']);
   const [options, setOptions] = useState(getLocalstorageOptions(SQL_LOGS_OPTIONS_CACHE_KEY));
+  const pageLoadMode = options.pageLoadMode || 'pagination';
   const [fields, setFields] = useState<string[]>([]);
-  const [serviceParams, setServiceParams] = useState({
+  const [serviceParams, setServiceParams, getServiceParams] = useGetState({
     current: 1,
     pageSize: 10,
   });
+  const [logs, setLogs] = useState<any[]>([]);
 
-  const updateOptions = (newOptions) => {
+  const updateOptions = (newOptions, reload?: boolean) => {
     const mergedOptions = {
       ...options,
       ...newOptions,
     };
     setOptions(mergedOptions);
-    setLocalstorageOptions(SQL_LOGS_OPTIONS_CACHE_KEY, mergedOptions);
+    if (reload) {
+      setLocalstorageOptions(SQL_LOGS_OPTIONS_CACHE_KEY, mergedOptions);
+      setServiceParams({
+        current: 1,
+        pageSize: 10,
+      });
+      const tableEleNodes = document.querySelectorAll(logsTableSelectors)[0];
+      tableEleNodes?.scrollTo(0, 0);
+    }
   };
 
   const service = () => {
@@ -71,6 +84,8 @@ function Raw(props: IProps) {
 
           const columnsKeys = getFieldsFromTableData(res.list || []);
           setFields(columnsKeys);
+
+          setLogs(_.slice(newLogs, 0, serviceParams.pageSize)); // 首次只加载一页数据
 
           return {
             list: newLogs,
@@ -107,35 +122,73 @@ function Raw(props: IProps) {
               timeField={queryValues?.time_field}
               hideHistogram
               loading={loading}
-              logs={_.slice(data?.list, (serviceParams.current - 1) * serviceParams.pageSize, serviceParams.current * serviceParams.pageSize) || []}
+              logs={logs}
               fields={fields}
               options={options}
               filterFields={(fieldKeys) => {
                 return filteredFields(fieldKeys, options.organizeFields);
               }}
               optionsExtraRender={
-                <Space size={0}>
-                  <Pagination
-                    size='small'
-                    total={data?.total}
-                    current={serviceParams.current}
-                    pageSize={serviceParams.pageSize}
-                    onChange={(current, pageSize) => {
-                      setServiceParams((prev) => ({
-                        ...prev,
-                        current,
-                        pageSize,
-                      }));
-                    }}
-                    showTotal={(total) => {
-                      return t('common:table.total', { total });
-                    }}
-                  />
-                  {IS_PLUS && <DownloadModal queryData={{ ...form.getFieldsValue(), total: data?.total }} />}
-                </Space>
+                pageLoadMode === 'pagination' ? (
+                  <Space size={0}>
+                    <Pagination
+                      size='small'
+                      total={data?.total}
+                      current={serviceParams.current}
+                      pageSize={serviceParams.pageSize}
+                      onChange={(current, pageSize) => {
+                        setServiceParams((prev) => ({
+                          ...prev,
+                          current,
+                          pageSize,
+                        }));
+                        const newLogs = _.slice(data?.list, (current - 1) * pageSize, current * pageSize) || [];
+                        setLogs(
+                          _.map(newLogs, (item) => {
+                            return {
+                              ...item,
+                              ___id___: _.uniqueId('log_id_'),
+                            };
+                          }),
+                        );
+                      }}
+                      showTotal={(total) => {
+                        return t('common:table.total', { total });
+                      }}
+                    />
+                    {IS_PLUS && <DownloadModal queryData={{ ...form.getFieldsValue(), total: data?.total }} />}
+                  </Space>
+                ) : (
+                  t('common:table.total', { total: data?.total })
+                )
               }
               onOptionsChange={updateOptions}
               showDateField={false}
+              onScrollCapture={() => {
+                const tableEleNodes = document.querySelectorAll(logsTableSelectors)[0];
+                if (tableEleNodes?.scrollHeight - (Math.round(tableEleNodes?.scrollTop) + tableEleNodes?.clientHeight) === 0) {
+                  // 滚动到底后加载下一页
+                  const currentServiceParams = getServiceParams();
+                  if (pageLoadMode === 'infiniteScroll' && data && logs.length < data.total) {
+                    setServiceParams((prev) => ({
+                      ...prev,
+                      current: currentServiceParams.current + 1,
+                    }));
+                    const appendLogs = _.slice(data.list, 0, (currentServiceParams.current + 1) * currentServiceParams.pageSize);
+                    setLogs(
+                      _.concat(
+                        logs,
+                        _.map(appendLogs, (item) => {
+                          return {
+                            ...item,
+                            ___id___: _.uniqueId('log_id_'),
+                          };
+                        }),
+                      ),
+                    );
+                  }
+                }
+              }}
             />
           </div>
         </div>
