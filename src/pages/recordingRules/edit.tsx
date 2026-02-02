5e7d8e34f8777f4d6885f65561a46cbd6ab7a47b
jsers <xqing9024@gmail.com>
2026-02-02 15:25:49 +0800

    refactor: recording rule form

--- src/pages/recordingRules/edit.tsx
+++ src/pages/recordingRules/edit.tsx
@@ -1,9 +1,11 @@
 import React, { useMemo, useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
+
 import PageLayout from '@/components/pageLayout';
 import { getRecordingRule } from '@/services/recording';
 import { useQuery } from '@/utils';
+
 import OperateForm from './components/operateForm';
 import './index.less';
 
@@ -16,19 +18,19 @@ const StrategyEdit: React.FC = () => {
     return params.id;
   }, [params]);
   const [curStrategy, setCurStrategy] = useState<any>({});
-  useEffect(() => {
-    getStrategy();
-    return () => {};
-  }, [strategyId]);
-
   const getStrategy = async () => {
     const res = await getRecordingRule(strategyId);
     setCurStrategy(res.dat || {});
   };
 
+  useEffect(() => {
+    getStrategy();
+    return () => {};
+  }, [strategyId]);
+
   return (
     <PageLayout title={t('title')} showBack>
-      {curStrategy.id && <OperateForm detail={curStrategy} type={!isClone ? 1 : 2} />}
+      {curStrategy.id && <OperateForm initialValues={curStrategy} type={!isClone ? 1 : 2} />}
     </PageLayout>
   );
 };
