10bbc49c9e1123557612a49eee2451e2eb831421
jsers <xqing9024@gmail.com>
2026-03-04 15:49:03 +0800

    refactor: redesign notification channel form with split-pane layout

--- src/pages/notificationChannels/constants.ts
+++ src/pages/notificationChannels/constants.ts
@@ -35,3 +35,98 @@ export const DEFAULT_VALUES = {
     },
   },
 };
+
+export const NOTIFICATION_CHANNEL_TYPES = {
+  flashduty: {
+    logo: '/image/logos/flashduty.png',
+    type: 'flashduty',
+  },
+  callback: {
+    logo: '/image/notification/http.png',
+    type: 'http',
+  },
+  email: {
+    logo: '/image/notification/smtp.png',
+    type: 'smtp',
+  },
+  dingtalk: {
+    logo: '/image/logos/dingtalk.png',
+    type: 'http',
+  },
+  wecom: {
+    logo: '/image/logos/wecom.png',
+    type: 'http',
+  },
+  feishucard: {
+    logo: '/image/logos/feishu.png',
+    type: 'http',
+  },
+  feishu: {
+    logo: '/image/logos/feishu.png',
+    type: 'http',
+  },
+  larkcard: {
+    logo: '/image/logos/feishu.png',
+    type: 'http',
+  },
+  lark: {
+    logo: '/image/logos/feishu.png',
+    type: 'http',
+  },
+  telegram: {
+    logo: '/image/logos/telegram.png',
+    type: 'http',
+  },
+  'ali-voice': {
+    logo: '/image/logos/alibabacloud.png',
+    type: 'http',
+  },
+  'ali-sms': {
+    logo: '/image/logos/alibabacloud.png',
+    type: 'http',
+  },
+  'tx-voice': {
+    logo: '/image/logos/tencentcloud.png',
+    type: 'http',
+  },
+  'tx-sms': {
+    logo: '/image/logos/tencentcloud.png',
+    type: 'http',
+  },
+  slackbot: {
+    logo: '/image/logos/slack.png',
+    type: 'http',
+  },
+  slackwebhook: {
+    logo: '/image/logos/slack.png',
+    type: 'http',
+  },
+  mattermostbot: {
+    logo: '/image/logos/mattermost.png',
+    type: 'http',
+  },
+  mattermostwebhook: {
+    logo: '/image/logos/mattermost.png',
+    type: 'http',
+  },
+  discord: {
+    logo: '/image/logos/discord.png',
+    type: 'http',
+  },
+  jsm_alert: {
+    logo: '/image/logos/jira.png',
+    type: 'http',
+  },
+  jira: {
+    logo: '/image/logos/jira.png',
+    type: 'http',
+  },
+  pagerduty: {
+    logo: '/image/logos/pagerduty.png',
+    type: 'pagerduty',
+  },
+  script: {
+    logo: '/image/notification/script.png',
+    type: 'script',
+  },
+};
--- src/pages/notificationChannels/pages/Add.tsx
+++ src/pages/notificationChannels/pages/Add.tsx
@@ -1,23 +1,45 @@
 import React from 'react';
 import { useTranslation } from 'react-i18next';
-import { useHistory } from 'react-router-dom';
-import { message } from 'antd';
+import { useHistory, useLocation } from 'react-router-dom';
+import { message, Space } from 'antd';
+import queryString from 'query-string';
 
 import PageLayout from '@/components/pageLayout';
 
-import { NS } from '../constants';
+import { NS, NOTIFICATION_CHANNEL_TYPES, DEFAULT_VALUES } from '../constants';
 import { postItems } from '../services';
+import { ChannelItem } from '../types';
 import { normalizeFormValues } from '../utils/normalizeValues';
 import Form from './Form';
 
 export default function Add() {
   const { t } = useTranslation(NS);
   const history = useHistory();
+  const query = queryString.parse(useLocation().search);
+  const ident = (query.ident as string) || 'callback';
+  const identConfig = NOTIFICATION_CHANNEL_TYPES[ident] ? NOTIFICATION_CHANNEL_TYPES[ident] : NOTIFICATION_CHANNEL_TYPES['callback'];
 
   return (
-    <PageLayout title={t('title')} showBack backPath={`/${NS}`} doc='https://flashcat.cloud/docs/content/flashcat-monitor/nightingale-v8/usecase/media/'>
+    <PageLayout
+      title={
+        <Space className='ml-2'>
+          <img src={identConfig.logo} alt={ident} height={18} />
+          {t(`types.${ident}`)}
+        </Space>
+      }
+      showBack
+      backPath={`/${NS}`}
+      doc='https://flashcat.cloud/docs/content/flashcat-monitor/nightingale-v8/usecase/media/'
+    >
       <div className='n9e'>
         <Form
+          initialValues={
+            {
+              ...(DEFAULT_VALUES as any),
+              ident,
+              request_type: identConfig.type,
+            } as ChannelItem
+          }
           onOk={(values) => {
             postItems([normalizeFormValues(values)]).then(() => {
               message.success(t('common:success.add'));
