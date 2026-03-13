7b92322fdf961fada63f845fdffd12a40f0236bd
jsers <xqing9024@gmail.com>
2026-03-13 13:39:13 +0800

    fix: resolve race condition in useFieldConfig and reuse RenderValue for time column

--- src/pages/logExplorer/components/RenderValue/useFieldConfig.tsx
+++ src/pages/logExplorer/components/RenderValue/useFieldConfig.tsx
@@ -1,3 +1,13 @@
+/**
+ * Changes:
+ *
+ * 2025-03-13
+ * 存在竞态条件，当 ES 数据源类型时 indexPatternId 还没准备好时值是错误的
+ * 会走 isPlus 分支，调用 searchDrilldown 接口
+ * 等 indexPatternId 准备好后，会调用 getESIndexPattern 接口
+ * 这两个接口 都会设置 fieldConfig，导致最终 fieldConfig 的值不确定
+ */
+
 import React, { useState, useEffect, useContext } from 'react';
 import _ from 'lodash';
 
@@ -12,9 +22,12 @@ import { searchDrilldown } from 'plus:/pages/LogExploreLinkSetting/services';
 export default function useFieldConfig(search: IFieldSearch, dep: any): FieldConfigVersion2 | undefined {
   const [fieldConfig, setFieldConfig] = useState<FieldConfigVersion2>();
   const { isPlus } = useContext(CommonStateContext);
+
   useEffect(() => {
+    let cancelled = false;
     if (search.cate === 'elasticsearch' && search.indexPatternId) {
       getESIndexPattern(search.indexPatternId).then((res) => {
+        if (cancelled) return;
         let fieldConfig;
         try {
           fieldConfig = standardizeFieldConfig(JSON.parse(res.fields_format));
@@ -30,6 +43,7 @@ export default function useFieldConfig(search: IFieldSearch, dep: any): FieldCon
       const isDorisAvailable = search.cate === 'doris' && search.resource;
       if (isESAvailable || isLokiAvailable || isSLSAvailable || isDorisAvailable) {
         searchDrilldown(search).then((res) => {
+          if (cancelled) return;
           if (res.length > 0) {
             try {
               let configs = JSON.parse(res[0].configs);
@@ -49,6 +63,9 @@ export default function useFieldConfig(search: IFieldSearch, dep: any): FieldCon
         });
       }
     }
+    return () => {
+      cancelled = true;
+    };
   }, [dep]);
   return fieldConfig;
 }
