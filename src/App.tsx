b9d5b97300424a4a3db201b3cd4091c19780d256
guguji5 <604821528@qq.com>
2026-03-24 14:39:09 +0800

    refactor: 处理license 过期 598 报错

--- src/App.tsx
+++ src/App.tsx
@@ -132,7 +132,7 @@ export interface ICommonState {
 export const basePrefix = import.meta.env.VITE_PREFIX || '';
 
 // 可以匿名访问的路由 TODO: job-task output 应该也可以匿名访问
-const anonymousRoutes = [`${basePrefix}/login`, `${basePrefix}/callback`, `${basePrefix}/share/alert-his-events/`];
+const anonymousRoutes = [`${basePrefix}/login`, `${basePrefix}/callback`, `${basePrefix}/share/alert-his-events/`, `${basePrefix}/system/license-management`];
 const anonymousRoutesNeedDataSource = [`${basePrefix}/chart`, `${basePrefix}/dashboards/share/`];
 // 判断是否是匿名访问的路由
 const anonymous = _.some(anonymousRoutes.concat(anonymousRoutesNeedDataSource), (route) => location.pathname.startsWith(route));
