22b8a088224b2a1298f753bce1872b290e25df8b
star-6 <290043518@qq.com>
2026-03-27 11:06:36 +0800

    fix type error

--- src/utils/useHook.ts
+++ src/utils/useHook.ts
@@ -5,4 +5,3 @@ type IGlobalVarType = {
   RangePickerHour?: string;
 };
 export const useGlobalVar = createGlobalState<IGlobalVarType>({});
-export const useParamsAiAction = createGlobalState<any>();
