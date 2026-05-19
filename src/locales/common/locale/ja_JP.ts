62a7a2e423ceed3d1081cf026b45d0dbdb8faa15
Fiona <baiyang.feng@outlook.com>
2026-05-19 16:06:49 +0800

    fix(common): update header doc link title

--- src/locales/common/locale/ja_JP.ts
+++ src/locales/common/locale/ja_JP.ts
@@ -18,6 +18,7 @@ const ja_JP = {
   unit: '単位',
   page_help: '使用説明',
   document_title: 'ドキュメント',
+  product_document_title: '製品ドキュメント',
   more_document_link: 'さらにドキュメント',
   and: 'および',
   yes: 'はい',
