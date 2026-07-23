378b5ba2a720995955f61f4498ffb22e7e9d01f6
jsers <xqing9024@gmail.com>
2026-07-23 11:16:24 +0800

    refactor(tailwind): add ease-smooth utility and fix font path
    - Add ease-smooth (cubic-bezier(0.2, 0, 0, 1)) to Tailwind theme
    - Replace inline easing in SideMenu with ease-smooth class
    - Fix font path from ../font/ to /font/ in theme/index.less

--- src/components/SideMenu/index.tsx
+++ src/components/SideMenu/index.tsx
@@ -430,7 +430,7 @@ const SideMenu = (props: SideMenuProps) => {
           className={cn(
             'relative z-20 flex h-full shrink-0 select-none flex-col justify-between border-0 border-r border-solid bg-sidebar',
             collapsed ? 'side-menu-collapsed-panel' : '',
-            !isResizingMenu && 'transition-all duration-300 ease-[cubic-bezier(0.2,0,0,1)]',
+            !isResizingMenu && 'transition-all duration-300 ease-smooth',
             !IS_ENT ? 'border-fc-300' : '',
           )}
           style={{
